714d19b5273187379c87ce635fe789afe3c27fe7
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-06-01 19:59:32 +0200

    Fixing document format

--- includes/design_pattern/spk_pool.hpp
+++ includes/design_pattern/spk_pool.hpp
@@ -2,30 +2,30 @@
 
 namespace spk
 {
-    template<typename TType>
-	class Pool
-	{	
-	private:
-        using Container = std::vector<TType*>;
+    template <typename TType>
+    class Pool
+    {
+    private:
+        using Container = std::vector<TType *>;
         Container _allocatedObjects;
 
-	public:
-		class Object
-		{
+    public:
+        class Object
+        {
             friend class Pool;
-		private:
-            Container* _source;
-            size_t *_referenceCount;
-            TType* _content;
-	
-			Object(Container* p_source, TType* p_content) :
-                _source(p_source),
-                _content(p_content),
-                _referenceCount(new size_t(1))
-            {
 
+        private:
+            Container *_source;
+            size_t *_referenceCount;
+            TType *_content;
+
+            Object(Container *p_source, TType *p_content) : _source(p_source),
+                                                            _content(p_content),
+                                                            _referenceCount(new size_t(1))
+            {
             }
-		public:
+
+        public:
             ~Object()
             {
                 (*_referenceCount)--;
@@ -34,51 +34,49 @@ namespace spk
                     _source->push_back(_content);
             }
 
-            Object(const Object& p_other) :
-                _source(p_other._source),
-                _content(p_other._content),
-                _referenceCount(p_other._referenceCount)
+            Object(const Object &p_other) : _source(p_other._source),
+                                            _content(p_other._content),
+                                            _referenceCount(p_other._referenceCount)
             {
                 (*_referenceCount)++;
             }
 
-			TType* operator()()
+            TType *operator()()
             {
                 return (_content);
             }
-			const TType* operator()() const 
+            const TType *operator()() const
             {
                 return (_content);
             }
-            
-            TType* operator->()
+
+            TType *operator->()
             {
                 return (_content);
             }
-			const TType* operator->() const 
+            const TType *operator->() const
             {
                 return (_content);
             }
-            
-            TType& operator*()
+
+            TType &operator*()
             {
                 return *_content;
             }
 
-            const TType& operator*() const
+            const TType &operator*() const
             {
                 return *_content;
             }
-		};
-        
-	public:
-		Pool()
+        };
+
+    public:
+        Pool()
         {
-
         }
-	
-		template <typename ... Args>
-		void reserve(size_t p_nbElement, Args&&... p_args)
+
+        template <typename... Args>
+        void reserve(size_t p_nbElement, Args &&...p_args)
         {
             size_t currentSize = _allocatedObjects.size();
 
@@ -88,9 +86,9 @@ namespace spk
                 _allocatedObjects[i] = new TType(std::forward<Args>(p_args)...);
             }
         }
-	
-		template <typename ... Args>
-		Object obtain(Args&&... p_args)
+
+        template <typename... Args>
+        Object obtain(Args &&...p_args)
         {
             if (_allocatedObjects.size() == 0)
             {
@@ -100,22 +98,19 @@ namespace spk
             }
             else
             {
-                TType* data = _allocatedObjects.back();
+                TType *data = _allocatedObjects.back();
                 _allocatedObjects.pop_back();
 
                 *data = TType(std::forward<Args>(p_args)...);
 
                 Object result = Object(&_allocatedObjects, data);
-                return (result);    
+                return (result);
             }
-
-            
         }
-	
-		size_t size() const
+
+        size_t size() const
         {
             return (_allocatedObjects.size());
         }
-	};
-
+    };
 }
