9a7bd33025315100b94596aea21d7ccadc26de7f
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-06-12 13:31:50 +0200

    Ending the Value implementation by adding Callback uppon edition definition

--- includes/design_pattern/spk_value.hpp
+++ includes/design_pattern/spk_value.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include "design_pattern/spk_contract_provider.hpp"
+
+#include "iostream/spk_iostream.hpp"
+
 namespace spk
 {
     template <typename TType>
-    class Value
+    class Value : public ContractProvider
     {
+        friend class Default;
+
     public:
         class Default
         {
@@ -12,49 +18,60 @@ namespace spk
 
         private:
             TType _value;
-            size_t _count;
 
-            void subscribe()
+            std::vector<Value<TType>*> _subscribers;
+
+            void subscribe(Value<TType>* p_subscriber)
             {
-                _count++;
+                _subscribers.push_back(p_subscriber);
             }
 
-            void unsubscribe()
+            void unsubscribe(Value<TType>* p_subscriber)
             {
-                _count--;
+                _subscribers.erase(std::remove(_subscribers.begin(), _subscribers.end(), p_subscriber), _subscribers.end());
+            }
+            
+            void _triggerSubscriberEditionCallbacks()
+            {
+                for (size_t i = 0; i < _subscribers.size(); i++)
+                {
+                    _subscribers[i]->_triggerEditionCallback();
+                }
             }
 
         public:
             template <typename ... Args>
             Default(Args&& ... p_args) :
                 _value(std::forward<Args>(p_args)...),
-                _count(0)
+                _subscribers()
             {
 
             }
             
             Default(const Default& p_other) :
                 _value(p_other._value),
-                _count(0)
+                _subscribers()
             {
 
             }
 
             ~Default() noexcept(false)
             {
-                if (_count != 0)
+                if (_subscribers.empty() == false)
                     throw std::runtime_error("Destroying a Default value still used by at least one Value");
             }
 
             Default& operator = (const TType& p_newValue)
             {
                 _value = p_newValue;
+                _triggerSubscriberEditionCallbacks();
                 return (*this);
             }
 
             Default& operator = (const Default& p_other)
             {
                 _value = p_other._value;
+                _triggerSubscriberEditionCallbacks();
                 return (*this);
             }
 
@@ -71,9 +88,18 @@ namespace spk
             Custom
         };
 
+        CallbackContainer _onEditionCallbacks;
         State _state;
         Default* _default;
         TType _value;
+
+        void _triggerEditionCallback()
+        {
+            for (size_t i = 0; i < _onEditionCallbacks.size(); i++)
+            {
+                _onEditionCallbacks[i]();
+            }
+        }
 
     public:
         Value(const Default& p_defaultValue) : 
@@ -93,14 +119,28 @@ namespace spk
             setDefaultValue(p_defaultValue);
         }
 
+        Value(const Value& p_other) : 
+            _default(p_other._default),
+            _value(p_other._value),
+            _state(p_other._state)
+        {
+            _default->subscribe(this);
+        }
+
         ~Value()
         {
-            _default->unsubscribe();
+            _default->unsubscribe(this);
+        }
+
+        Contract subscribe(Callback p_callback)
+        {
+            return (std::move(ContractProvider::subscribe(_onEditionCallbacks, p_callback)));
         }
         
         void reset()
         {
             _state = State::Default;
+            _triggerEditionCallback();
         }
 
         void setDefaultValue(const Default& p_defaultValue)
@@ -108,16 +148,26 @@ namespace spk
             Default* tmp = const_cast<Default*>(&p_defaultValue);
 
             if (_default != nullptr)
-                _default->unsubscribe();
+                _default->unsubscribe(this);
             _default = tmp;
             if (_default != nullptr)
-                _default->subscribe();
+                _default->subscribe(this);
         }
 
         Value& operator = (const Default& p_defaultValue)
         {
             setDefaultValue(p_defaultValue);
             _state = State::Default;
+            _triggerEditionCallback();
+            return (*this);
+        }
+
+        Value& operator = (const Value& p_other)
+        {
+            setDefaultValue(*p_other._default);
+            _value = p_other._value;
+            _state = p_other._state;
+            _triggerEditionCallback();
             return (*this);
         }
 
@@ -125,6 +175,7 @@ namespace spk
         {
             _value = p_newValue;
             _state = State::Custom;
+            _triggerEditionCallback();
             return (*this);
         }
 
