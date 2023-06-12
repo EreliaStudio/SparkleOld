6d9235d169759c20caa361587705b78c6f550a9c
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-06-12 13:38:53 +0200

    Editing the test main to be sure Value are working correctly

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,97 +1,54 @@
 #include "playground.hpp"
+#include <assert.h>
 
-spk::Value<int>::Default defaultA = 42;
-spk::Value<int>::Default defaultB = 66;
+int main() {
+    // Testing Default constructor
+    spk::Value<int>::Default defaultValue(10);
+    assert(static_cast<int>(defaultValue) == 10);
 
-spk::Value<int> valueA = defaultA;
-spk::Value<int> valueB = spk::Value<int>(defaultA);
-spk::Value<int> valueC = spk::Value<int>(defaultA, 100);
+    // Testing Default copy constructor
+    spk::Value<int>::Default copiedDefaultValue(defaultValue);
+    assert(static_cast<int>(copiedDefaultValue) == 10);
 
-void printValues(size_t p_nbRun, std::wstring p_message)
-{
-	spk::cout << " --- RUN " << p_nbRun << " : " << p_message << " ---" << std::endl;
-	spk::cout << "DefaultA : " << defaultA << std::endl;
-	spk::cout << "DefaultB : " << defaultB << std::endl;
-	spk::cout << "ValueA : " << valueA << std::endl;
-	spk::cout << "ValueB : " << valueB << std::endl;
-	spk::cout << "ValueC : " << valueC << std::endl;
-	spk::cout << " ---" << std::endl;
-}
+    // Testing Value constructor with Default
+    spk::Value<int> valueFromDefault(defaultValue);
+    assert(static_cast<int>(valueFromDefault) == 10);
 
-int main()
-{
-	defaultA = 42;
-	defaultB = 66;
+    // Testing Value copy constructor
+    spk::Value<int> copiedValue(valueFromDefault);
+    assert(static_cast<int>(copiedValue) == 10);
 
-	valueA = defaultA;
-	valueB = spk::Value<int>(defaultA);
-	valueC = spk::Value<int>(defaultA, 100);
+    // Testing Value constructor with Default and custom value
+    spk::Value<int> customValue(defaultValue, 20);
+    assert(static_cast<int>(customValue) == 20);
 
-	printValues(0, L"Initial state");
+    // Testing Value assignment operator with Default
+    defaultValue = 15;
+    valueFromDefault = defaultValue;
+    assert(static_cast<int>(valueFromDefault) == 15);
 
-	valueA = 15;
+    // Testing Value assignment operator with Value
+    copiedValue = customValue;
+    assert(static_cast<int>(copiedValue) == 20);
 
-	printValues(1, L"Setting valueA");
+    // Testing Value assignment operator with TType
+    valueFromDefault = 30;
+    assert(static_cast<int>(valueFromDefault) == 30);
 
-	valueC = valueA + valueB;
+    // Testing Value reset function
+    valueFromDefault.reset();
+    assert(static_cast<int>(valueFromDefault) == 15);
 
-	printValues(2, L"Checking sum operation");
+    // Test callback subscription and triggering
+    bool callbackTriggered = false;
+    auto callback = [&callbackTriggered]() { callbackTriggered = true; };
 
-	valueA.reset();
+    // Subscribe to callback
+    auto contract = valueFromDefault.subscribe(callback);
 
-	printValues(3, L"Reseting value A");
+    // Change value to trigger callback
+    valueFromDefault = 35;
+    assert(callbackTriggered == true);
 
-	defaultA = 1337;
-
-	printValues(4, L"Editing default value A");
-
-	valueA.setDefaultValue(defaultB);
-
-	printValues(5, L"Setting value A default value to B");
-
-	if (true)
-	{
-		spk::Value<int>::Contract tmpContractA = valueA.subscribe([&](){
-			spk::cout << "ValueA edition" << std::endl;
-		});
-		spk::Value<int>::Contract tmpContractB = valueB.subscribe([&](){
-			spk::cout << "ValueB edition" << std::endl;
-		});
-		spk::Value<int>::Contract tmpContractC = valueC.subscribe([&](){
-			spk::cout << "ValueC edition" << std::endl;
-		});
-
-		spk::cout << "Step 1" << std::endl;
-
-		valueA = 15;
-
-		spk::cout << "Step 2" << std::endl;
-		
-		valueB = 64;
-
-		spk::cout << "Step 3" << std::endl;
-		
-		valueC = 128;
-
-		spk::cout << "Step 4" << std::endl;
-		
-		valueA.reset();
-
-		spk::cout << "Step 5" << std::endl;
-
-		valueB = valueA;
-
-		spk::cout << "Step 6" << std::endl;
-
-		valueC = defaultB;
-
-		spk::cout << "Step 7" << std::endl;
-
-		defaultB = 0;
-
-		spk::cout << "Step 8" << std::endl;
-		
-	}
-
-	return (0);
+    return 0;
 }
