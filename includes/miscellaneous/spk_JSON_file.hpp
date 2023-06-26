4d74d8e726997cbbec10f358a3c95340b0cc6837
jrouchon <justinerobin24@hotmail.fr>
2023-06-27 00:07:11 +0200

    Implementing the json object structure, with getter and adder to the object structure

--- includes/miscellaneous/spk_JSON_file.hpp
+++ includes/miscellaneous/spk_JSON_file.hpp
@@ -14,6 +14,7 @@ namespace spk
 {
     namespace JSON
     {
+
         class Unit
         {
         private:
@@ -22,15 +23,14 @@ namespace spk
         public:
             Unit() : _data(false) {}
 
-            template<typename TType>
-            void set(const TType& p_type)
+            template <typename TType>
+            void set(const TType &p_type)
             {
                 _data = p_type;
             }
 
-
-            template<typename TType>
-            TType as() const 
+            template <typename TType>
+            TType as() const
             {
                 return std::get<TType>(_data);
             }
@@ -39,7 +39,7 @@ namespace spk
         class Object
         {
         public:
-            using Attribute = std::variant<Unit, std::vector<Object>>;
+            using Attribute = std::variant<Unit, std::vector<Unit>, Object, std::vector<Object>>;
 
         private:
             std::map<std::wstring, Attribute> _attributes;
@@ -47,47 +47,109 @@ namespace spk
         public:
             Object()
             {
-
+                
             }
 
-            void addAttribute(const std::wstring& p_name, const Unit& p_value)
+            void addAttribute(const std::wstring &p_name, const Unit &p_value)
             {
                 _attributes[p_name] = p_value;
             }
 
-            void addAttribute(const std::wstring& p_name, const Object& p_object)
+            void addAttribute(const std::wstring &p_name, const std::vector<Unit> &p_values)
             {
-                std::vector<Object> tmpArray;
-
-                tmpArray.push_back(p_object);
-                _attributes[p_name] = tmpArray;
+                _attributes[p_name] = p_values;
             }
 
-            void addAttribute(const std::wstring& p_name, const std::vector<Object>& p_objects)
+            void addAttribute(const std::wstring &p_name, const Object &p_object)
+            {
+                _attributes[p_name] = p_object;
+            }
+
+            void addAttribute(const std::wstring &p_name, const std::vector<Object> &p_objects)
             {
                 _attributes[p_name] = p_objects;
             }
 
-            template<typename TType>
-            void addAttribute(const std::wstring& p_name, const TType& p_data)
+            void addAttribute(const std::wstring &p_name)
             {
-                Unit toAdd;
-
-                toAdd.set<TType>(p_data);
-                _attributes[p_name] = toAdd;
+                _attributes[p_name] = Unit();
             }
 
-            const Unit& get(const std::wstring& p_name) const
+            template <typename TType>
+            void addAttribute(const std::wstring &p_name, const TType& p_value)
+            {
+                _attributes[p_name] = Unit();
+                std::get<Unit>(_attributes[p_name]).set<TType>(p_value);
+            }
+
+            void addAttributeArray(const std::wstring &p_name)
+            {
+                _attributes[p_name] = std::vector<Unit>();
+            }
+
+            void addObject(const std::wstring &p_name)
+            {
+                _attributes[p_name] = Object();
+            }
+
+            void addObjectArray(const std::wstring &p_name)
+            {
+                _attributes[p_name] = std::vector<Object>();
+            }
+
+            Unit &get(const std::wstring &p_name)
             {
                 if (_attributes.count(p_name) == 0)
                     spk::throwException(L"Can't access to attribute [" + p_name + L"] inside JSON::Object");
                 return (std::get<Unit>(_attributes.at(p_name)));
             }
 
-            const std::vector<Object>& operator[] (const std::wstring& p_name) const
+            std::vector<Unit> &getArray(const std::wstring &p_name)
+            {
+                if (_attributes.count(p_name) == 0)
+                    spk::throwException(L"Can't access to attribute array [" + p_name + L"] inside JSON::Object");
+                return (std::get<std::vector<Unit>>(_attributes.at(p_name)));
+            }
+
+            Object &getObject(const std::wstring &p_name)
+            {
+                if (_attributes.count(p_name) == 0)
+                    spk::throwException(L"Can't access to object [" + p_name + L"] inside JSON::Object");
+                return (std::get<Object>(_attributes.at(p_name)));
+            }
+
+            std::vector<Object> &getObjectArray(const std::wstring &p_name)
+            {
+                if (_attributes.count(p_name) == 0)
+                    spk::throwException(L"Can't access to object array [" + p_name + L"] inside JSON::Object");
+                return (std::get<std::vector<Object>>(_attributes.at(p_name)));
+            }
+
+            const Unit &get(const std::wstring &p_name) const
             {
                 if (_attributes.count(p_name) == 0)
                     spk::throwException(L"Can't access to attribute [" + p_name + L"] inside JSON::Object");
+                return (std::get<Unit>(_attributes.at(p_name)));
+            }
+
+            const std::vector<Unit> &getArray(const std::wstring &p_name) const
+            {
+                if (_attributes.count(p_name) == 0)
+                    spk::throwException(L"Can't access to attribute array [" + p_name + L"] inside JSON::Object");
+                return (std::get<std::vector<Unit>>(_attributes.at(p_name)));
+            }
+
+            const Object &getObject(const std::wstring &p_name) const
+            {
+                if (_attributes.count(p_name) == 0)
+                    spk::throwException(L"Can't access to object [" + p_name + L"] inside JSON::Object");
+                return (std::get<Object>(_attributes.at(p_name)));
+            }
+
+            const std::vector<Object> &getObjectArray(const std::wstring &p_name) const
+            {
+                if (_attributes.count(p_name) == 0)
+                    spk::throwException(L"Can't access to object array [" + p_name + L"] inside JSON::Object");
                 return (std::get<std::vector<Object>>(_attributes.at(p_name)));
             }
         };
@@ -95,7 +157,6 @@ namespace spk
         class File
         {
         private:
-
         public:
             File() = default;
 
