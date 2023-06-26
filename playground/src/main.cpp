82875903b65b0a3a1326c2b006aec49d147baf84
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-06-26 20:09:19 +0200

    The Thread::_starterSignal become a promise<void>

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,35 +1,14 @@
 #include "playground.hpp"
 
-spk::Promise<int> promise;
-
-void callback()
-{
-	spk::cout << "Callback called" << std::endl;
-}
-
-void promiseTests()
-{
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-	promise.setValue(42);
-	try
-	{
-		promise.setValue(42);
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-}
 
 int main()
 {
-	auto contract = promise.subscribe(callback);
-	auto second_contract = promise.subscribe([]() { spk::cout << L"Second callback called" << std::endl; });
+	spk::Thread thread(spk::Thread::LaunchMethod::Delayed, L"Thread", []()
+	{
+		spk::cout << L"Hello world!" << std::endl;
+	});
+	thread.start();
+	thread.join();
 
-	spk::Thread thread(spk::Thread::LaunchMethod::Immediate, L"Promise test", promiseTests);
-	contract.resign();
-
-	spk::cout << L"Promise value: " << promise.value() << std::endl;
-	spk::cout << L"Promise value: " << promise.value() << std::endl;
 	return 0;
 }
