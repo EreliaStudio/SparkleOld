cb27fa786bd243c1ab48f3b27fd30bc96fc02e89
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-06-27 11:19:55 +0200

    🧑‍💻 Improvement of function readability.

--- src/system/spk_chronometer.cpp
+++ src/system/spk_chronometer.cpp
@@ -20,18 +20,18 @@ namespace spk
 
 	const long long& Chronometer::duration() const
 	{
-		if (_start == 0)
-			return (_duration);
-		_duration = spk::TimeMetrics::instance()->time() - _start;
+		if (_start != 0)
+			_duration = spk::TimeMetrics::instance()->time() - _start;
 		return (_duration);
 	}
 
 	const long long& Chronometer::stop()
 	{
-		if (_start == 0)
-			return (_duration);
-		_duration = spk::TimeMetrics::instance()->time() - _start;
-		_start = 0;
+		if (_start != 0)
+		{
+			_duration = spk::TimeMetrics::instance()->time() - _start;
+			_start = 0;
+		}
 		return (_duration);
 	}
 }
