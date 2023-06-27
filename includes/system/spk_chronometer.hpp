e1daec74651b532561a71f90823afc0b4f467aec
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-06-27 10:23:22 +0200

    📝 Comment of spk::Chronometer.

--- includes/system/spk_chronometer.hpp
+++ includes/system/spk_chronometer.hpp
@@ -15,8 +15,24 @@ namespace spk
 		Chronometer();
 		~Chronometer();
 
+		/**
+		 * @brief Start the chronometer and reset the duration
+		 */
 		void start();
+		
+		/**
+		 * @brief Get the duration since the start of the chronometer
+		 * @return The duration in milliseconds
+		 * @note Return 0 if the chronometer is not started.
+		 * @note If stop has been called, the duration is not updated.
+		 */
 		const long long& duration();
+
+		/**
+		 * @brief Stop the chronometer and return the duration
+		 * @return The duration in milliseconds
+		 * @note Return 0 if the chronometer is not started
+		 */
 		const long long& stop();
 	};
 }
