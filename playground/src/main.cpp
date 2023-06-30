76525da2b433706deaeff5137ded25b2c5a2cd94
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-06-30 17:27:13 +0200

    ✨✅ Improvement of Memento

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,53 +1,118 @@
 #include "playground.hpp"
 
-void MementoTest()
+class Object
 {
-	std::wstring str(L"Hello, World!");
+private:
+	int _toRemember;
+	spk::Vector2Int _toRememberToo;
 
-	spk::Memento<std::wstring> memento(str);
+	int _notImportant;
 
-	spk::Memento<std::wstring>::Snapshot* snapshot = memento.takeSnapshot();
+public:
+	Object(int p_toRemember, spk::Vector2Int p_toRememberToo, int p_notImportant):
+		_toRemember(p_toRemember),
+		_toRememberToo(p_toRememberToo),
+		_notImportant(p_notImportant)
+	{
+	}
 
-	spk::cout << L"Begin: str = " << str << std::endl;
+	friend std::wostream& operator<<(std::wostream& p_os, const Object& p_object)
+	{
+		p_os << L"Object: " << p_object._toRemember << L", " << p_object._toRememberToo << L", " << p_object._notImportant;
+		return p_os;
+	}
 
-	str = L"Goodbye, World!";
-	auto secondSnapshot = memento.takeSnapshot();
+	friend spk::DataBuffer& operator<<(spk::DataBuffer& p_buffer, const Object& p_object)
+	{
+		p_buffer << p_object._toRemember << p_object._toRememberToo;
+		return p_buffer;
+	}
 
-	spk::cout << L"\tAssignation: str = " << str << std::endl;
+	friend spk::DataBuffer& operator>>(spk::DataBuffer& p_buffer, Object& p_object)
+	{
+		p_buffer >> p_object._toRemember >> p_object._toRememberToo;
+		return p_buffer;
+	}
+};
 
-	memento.restoreSnapshot(snapshot);
+template <typename TType>
+void updateValue(spk::Memento<TType>& p_memento,
+	TType& p_value, const TType& p_newValue)
+{
+	p_value = p_newValue;
+	p_memento.save(p_value);
+	spk::cout << L"Saved at: " << p_value << std::endl;
+}
 
-	spk::cout << L"\t\tRestored: str = " << str << std::endl;
+template <typename TType>
+void undoValue(spk::Memento<TType>& p_memento, TType& p_value)
+{
+	p_memento.undo(p_value);
+	spk::cout << L"\tUndo: " << p_value << std::endl;
+}
 
-	str = L"Whatev, World!";
+template <typename TType>
+void redoValue(spk::Memento<TType>& p_memento, TType& p_value)
+{
+	p_memento.redo(p_value);
+	spk::cout << L"\tRedo: " << p_value << std::endl;
+}
 
-	spk::cout << L"\tAssignation: str = " << str << std::endl;
+void intMementoTest()
+{
+	spk::cout << L"\nIntMementoTest" << std::endl;
 
-	memento.restoreSnapshot(snapshot);
+	spk::Memento<int> memento;
+	int state = 0;
 
-	spk::cout << L"\t\tRestored: str = " << str << std::endl;
+	updateValue(memento, state, 1);
+	updateValue(memento, state, 2);
 
-	memento.restoreSnapshot(secondSnapshot);
+	spk::cout << L"Current state: " << state << std::endl;
 
-	spk::cout << L"\t\tRestored: str = " << str << std::endl;
+	undoValue(memento, state);
+	try
+	{
+		undoValue(memento, state);
+	}
+	catch (const std::exception& e)
+	{
+		spk::cout << L"\t\t" << e.what() << std::endl;
+	}
+	redoValue(memento, state);
 
-	spk::Memento<std::wstring>::Snapshot standaloneSnapshot;
+	undoValue(memento, state);
+	redoValue(memento, state);
+	try
+	{
+		redoValue(memento, state);
+	}
+	catch (const std::exception& e)
+	{
+		spk::cout << L"\t\t" << e.what() << std::endl;
+	}
+}
 
-	standaloneSnapshot.save(&str);
+void customMemtoTest()
+{
+	spk::Memento<Object> memento;
+	Object state(0, {0, 0}, 0);
 
-	str = L"Ahahah";
+	updateValue(memento, state, Object(1, {1, 1}, 1));
+	memento.reset();
+	updateValue(memento, state, Object(2, {2, 2}, 2));
+	updateValue(memento, state, Object(3, {3, 3}, 3));
 
-	spk::cout << L"\tAssignation: str = " << str << std::endl;
+	spk::cout << L"Current state: " << state << std::endl;
+	undoValue(memento, state);
 
-	// standaloneSnapshot.load(&str); // Both work.
-	memento.restoreSnapshot(&standaloneSnapshot);
-
-	spk::cout << L"\t\tRestored: str = " << str << std::endl;
+	redoValue(memento, state);
 }
 
 int main()
 {
-	MementoTest();
+	intMementoTest();
+	customMemtoTest();
 
 	return 0;
 }
