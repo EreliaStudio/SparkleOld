03694669291e86b2d7f762b0570d3d94cd360088
jrouchon <justinerobin24@hotmail.fr>
2023-06-30 11:20:03 +0200

    Removing the unnecessary Object* outside of the class

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -16,7 +16,7 @@ public:
 		_initialized = false;
 	}
 
-	Object* addAttribute(const std::wstring& p_key)
+	Object& addAttribute(const std::wstring& p_key)
 	{
 		if (_initialized == false)
 		{
@@ -26,7 +26,7 @@ public:
 
 		Object* result = new Object();
 		std::get<std::map<std::wstring, Object*>>(_content)[p_key] = result;
-		return (result);
+		return (*result);
 	}
 
 	void list()
@@ -46,63 +46,75 @@ public:
 		spk::cout << std::endl;
 	}
 
-	Object* operator [] (const std::wstring& p_key)
-	{
-		Object* result = std::get<std::map<std::wstring, Object*>>(_content).at(p_key);
-
-		if (std::holds_alternative<Unit>(result->_content) == false)
-			return (result);
-		else
-		{
-			if (std::holds_alternative<Object *>(std::get<Unit>(result->_content)) == false)
-				return (result);
-			else	
-				return (result->as<Object*>());
-		}
-	}
-
-	const Object* operator [] (const std::wstring& p_key) const
-	{
-		Object* result = std::get<std::map<std::wstring, Object*>>(_content).at(p_key);
-
 	/*
-		Forced to add this to prevent user from writing as<Object*>() after getting an object from the map
+		Forced to do this to prevent user from writing as<Object*>() after getting an object from the map
 		it's not mandatory, but it's easier to read for the user with this.
 
 		If not keeped, we coult simply :
 		return (std::get<std::map<std::wstring, Object*>>(_content).at(p_key));
 	*/
+	Object& operator [] (const std::wstring& p_key)
+	{
+		Object* result = std::get<std::map<std::wstring, Object*>>(_content).at(p_key);
+
 		if (std::holds_alternative<Unit>(result->_content) == false)
-			return (result);
+			return (*(result));
 		else
 		{
 			if (std::holds_alternative<Object *>(std::get<Unit>(result->_content)) == false)
-				return (result);
+				return (*(result));
 			else	
-				return (result->as<Object*>());
+				return (*(result->as<Object*>()));
 		}
 	}
 
-	Object* push_back()
+	const Object& operator [] (const std::wstring& p_key) const
+	{
+		Object* result = std::get<std::map<std::wstring, Object*>>(_content).at(p_key);
+
+		if (std::holds_alternative<Unit>(result->_content) == false)
+			return (*(result));
+		else
+		{
+			if (std::holds_alternative<Object *>(std::get<Unit>(result->_content)) == false)
+				return (*(result));
+			else	
+				return (*(result->as<Object*>()));
+		}
+	}
+
+	Object& append()
 	{
 		if (_initialized == false)
 		{
 			_content = std::vector<Object*>();
 			_initialized = true;
 		}
+
 		Object* result = new Object();
 		std::get<std::vector<Object*>>(_content).push_back(result);
-		return (result);
+		return (*result);
 	}
 
-	Object* operator[](size_t p_index)
+	void push_back(Object& p_object)
 	{
-		return (std::get<std::vector<Object*>>(_content)[p_index]);
+		if (_initialized == false)
+		{
+			_content = std::vector<Object*>();
+			_initialized = true;
+		}
+		Object* result = new Object(p_object);
+		std::get<std::vector<Object*>>(_content).push_back(result);
 	}
 
-	const Object* operator[](size_t p_index) const
+	Object& operator[](size_t p_index)
 	{
-		return (std::get<std::vector<Object*>>(_content)[p_index]);
+		return (*(std::get<std::vector<Object*>>(_content)[p_index]));
+	}
+
+	const Object& operator[](size_t p_index) const
+	{
+		return (*(std::get<std::vector<Object*>>(_content)[p_index]));
 	}
 
 	size_t size() const 
@@ -115,7 +127,8 @@ public:
 		return (std::get<std::map<std::wstring, Object*>>(_content).count(p_key));
 	}
 
-	template<typename TType>
+	template<typename TType,
+             typename std::enable_if<!std::is_same<TType, Object>::value, int>::type = 0>
 	void set(const TType& p_value)
 	{
 		if (_initialized == false)
@@ -127,6 +140,14 @@ public:
 		std::get<Unit>(_content) = p_value;
 	}
 
+	template<typename TType,
+			 typename std::enable_if<std::is_same<TType, Object>::value, int>::type = 0>
+	void set(const TType& p_value)
+	{
+		Object* tmpObject = new Object(p_value);
+		set<Object*>(tmpObject);
+	}
+
 	template<typename TType>
 	const TType& as()
 	{
@@ -136,38 +157,32 @@ public:
 
 int main()
 {
-	Object* weapon = new Object();
-	weapon->addAttribute(L"Name")->set<std::wstring>(L"Sword");
-	weapon->addAttribute(L"Damage")->set<int>(10);
+	Object weapon = Object();
+	weapon.addAttribute(L"Name").set<std::wstring>(L"Sword");
+	weapon.addAttribute(L"Damage").set<int>(10);
 
 	Object tmp;
 
 	spk::cout << __LINE__ << std::endl;
 
-	Object* objectA = tmp.push_back();
-	Object* objectB = tmp.push_back();
-	Object* objectC = tmp.push_back();
+	Object& objectA = tmp.append();
+	Object& objectB = tmp.append();
+	Object& objectC = tmp.append();
 
-	spk::cout << __LINE__ << std::endl;
+	objectA.set<int>(10);
+	objectB.set<std::wstring>(L"Ceci est un test");
 
-	objectA->set<int>(10);
-	spk::cout << __LINE__ << std::endl;
-	objectB->set<std::wstring>(L"Ceci est un test");
-	spk::cout << __LINE__ << std::endl;
-
-	spk::cout << __LINE__ << std::endl;
-	objectC->addAttribute(L"Name")->set<std::wstring>(L"NameOfTheObject"); 
-	spk::cout << __LINE__ << std::endl;
-	objectC->addAttribute(L"Level")->set<int>(10);
-	spk::cout << __LINE__ << std::endl;
-	objectC->addAttribute(L"Weapon")->set<Object*>(weapon);
-	spk::cout << __LINE__ << std::endl;
+	objectC.addAttribute(L"Name").set<std::wstring>(L"NameOfTheObject"); 
+	objectC.addAttribute(L"Level").set<int>(10);
+	objectC.addAttribute(L"Weapon").push_back(weapon);
+	objectC[L"Weapon"].push_back(weapon);
 
 	spk::cout << "tmp A : " << std::endl;
-	spk::cout << "tmp[0] = " << tmp[0]->as<int>() << std::endl;
-	spk::cout << "tmp[1] = " << tmp[1]->as<std::wstring>() << std::endl;
-	spk::cout << "tmp[2] = " << tmp[2]->operator[](L"Name")->as<std::wstring>() << " / " << tmp[2]->operator[](L"Level")->as<int>() << std::endl;
-	spk::cout << "         and is equiped with -> " << tmp[2]->operator[](L"Weapon")->operator[](L"Name")->as<std::wstring>() << std::endl;
+	spk::cout << "tmp[0] = " << tmp[0].as<int>() << std::endl;
+	spk::cout << "tmp[1] = " << tmp[1].as<std::wstring>() << std::endl;
+	spk::cout << "tmp[2] = " << tmp[2][L"Name"].as<std::wstring>() << " / " << tmp[2][L"Level"].as<int>() << std::endl;
+	spk::cout << "         and is equiped with " << tmp[2][L"Weapon"][0][L"Name"].as<std::wstring>() << std::endl;
+	spk::cout << "                         and " << tmp[2][L"Weapon"][1][L"Name"].as<std::wstring>() << std::endl;
 
 	return (0);
 }
