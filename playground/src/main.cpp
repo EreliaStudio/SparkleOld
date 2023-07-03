23e7d7ae6b372e59ca81daae64cd3ba648620c00
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-07-03 12:30:36 +0200

    🚚🧪 Add many json test files.

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
-	return 0;
+	spk::JSON::File file;
+	std::wstring filesName[6] = {
+		 L"array", L"literal",
+		 L"number", L"object",
+		 L"string", L"test_json"
+	};
+
+	for (unsigned short i(0); i < 6; ++i)
+	{
+		file.load(L"json/" + filesName[i] + L".json");
+		spk::cout << "File number [" << i << "]: " << std::endl
+			<< file << std::endl;
+	}
+
+	return (0);
 }
