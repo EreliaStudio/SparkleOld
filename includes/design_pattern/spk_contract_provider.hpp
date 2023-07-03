c3f65e7277cc57d46d4a529ce36e8d6443f5daad
Neyune <ereali@student.42.fr>
2023-07-03 18:15:42 +0200

    Comment all design patern except value

--- includes/design_pattern/spk_contract_provider.hpp
+++ includes/design_pattern/spk_contract_provider.hpp
@@ -7,12 +7,19 @@
 
 namespace spk
 {
+	/**
+	 * @brief Construct a Contract with a callback function and a callbackContainer.
+	 * 
+	 */
 	class ContractProvider
 	{
 	public:
 		using Callback = std::function<void()>;
 		using CallbackContainer = std::deque<Callback>;
 
+		/**
+		 * @brief  Represent the ownership of a callback and allow users to interact with it.
+		 */
 		class Contract
 		{
 			friend class ContractProvider;
@@ -22,23 +29,69 @@ namespace spk
 			CallbackContainer &_callbackOwner;
 			Callback &_callback;
 
+			/**
+			 * @brief Construct a new Contract object.
+			 * 
+			 * @param p_callbackOwner The container that store the Callback.
+			 * @param p_callback The function to register in the Contract.
+			 */
 			Contract(CallbackContainer &p_callbackOwner, Callback &p_callback);
+			
+			/**
+			 * @brief Disable the construction of a new Contract object by copy.
+			 * @param p_other 
+			 */
 			Contract(const Contract &p_other) = delete;
 
+			/**
+			 *  false if has been resign or constructed by move else it's true.
+			 */
 			bool isOriginal();
 
 		public:
+			
+			/**
+			 * @brief Constructor by move , move a Contract object.
+			 * Set isOriginal to false.
+			 * @param p_other 
+			 */
 			Contract(Contract &&p_other);
-
+			
+			/**
+			* @brief Move assignment operator.
+			* 
+			* @param p_other The Contract object to move.
+			* @return Contract& The moved Contract object.
+			*/
+			Contract& operator=(Contract &&p_other) noexcept;
+		
+			/**
+			 * @brief Destroy the Contract object.
+			 * resign and set isOriginal to false.
+			 */
 			~Contract();
-
+			
+			/**
+			 * @brief Modifie the Callback function.
+			 * @param p_callback The new callback function.
+			 */
 			void edit(const Callback& p_callback);
-
+			
+			/**
+			 * @brief Erase the Contract callback function.
+			 */
 			void resign();
 		};
 
 	private:
 	protected:
+		/**
+		 * @brief Add the callback inside the container and create a Contract representing it.
+		 * 
+		 * @param p_callbackOwner The container that store the Callback.
+		 * @param p_callback The function to register in the Contract.
+		 * @return Contract 
+		 */
 		Contract subscribe(CallbackContainer& p_callbackOwner, const Callback& p_callback);
 	};
 }
