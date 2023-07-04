d5b21205e0ce175f1ca1142050f398cbe33bcfc4
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-07-04 17:06:27 +0200

    Editing singleton to throw error uppon multiples instanciate calls

--- includes/design_pattern/spk_singleton.hpp
+++ includes/design_pattern/spk_singleton.hpp
@@ -27,7 +27,25 @@ namespace spk
 
         static inline std::shared_ptr<TType> _instance = nullptr; /**< The shared pointer to the singleton instance. */
 
-        static inline std::shared_ptr<TType> _instance = nullptr; /**< The shared pointer to the singleton instance. */
+    public:
+        /**
+         * @brief Instantiate the singleton object.
+         * This template function creates a new instance of the singleton object if it doesn't exist.
+         * If an instance already exists, it returns the existing instance.
+         *
+         * @tparam Args The types of the arguments to be passed to the constructor of the singleton object.
+         * @param args The arguments to be passed to the constructor of the singleton object.
+         * @return The shared pointer to the singleton instance.
+         */
+        template <typename... Args>
+        static constexpr std::shared_ptr<TType> instanciate(Args &&...args)
+        {
+            if (_instance != nullptr)
+                throw std::runtime_error("Can't instanciate an already instancied singleton");
+            
+            _instance = std::shared_ptr<TType>(new TType(std::forward<Args>(args)...));
+            return _instance;
+        }
 
 	public:
 		template <typename... Args>
