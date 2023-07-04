1cd52b9c5442afeec730bb1a7e45d9c8d1735b21
Neyune <ereali@student.42.fr>
2023-07-04 19:21:32 +0200

    Create profiler

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,4 +1,11 @@
 #include "playground.hpp"
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <iostream>
+#include <cstdlib>
 
 class Memento
 {
@@ -67,57 +74,57 @@ struct MyStruct : Memento
 	int value;
 };
 
+    class Profiler
+    {
+        private:
+		std::map<std::string, spk::Chronometer> _chronometers;
+
+		public:
+        void startChronometer(const std::string & p_name)
+		{
+			if (_chronometers.count(p_name) != 0 && _chronometers[p_name].duration() != 0)
+				{
+					 throw std::runtime_error("Tried to start an already active Chronometer");
+				}
+			_chronometers[p_name].start();
+		}
+
+        const long long stopChronometer(const std::string & p_name)
+		{
+			if (_chronometers.count(p_name) == 0 )
+			{
+					throw std::runtime_error("This Chronometer does not exist ");
+			}
+			if (_chronometers[p_name].duration() == 0)
+			{
+					throw std::runtime_error("This Chronometer is not started ");
+			}		
+			return(_chronometers[p_name].stop());
+		}
+		
+    };
+
 int main()
 {
-	spk::StandardValue<int> tmp;
+	Profiler test;
 
-	tmp = -1;
-	for (size_t i = 0; i < 4; i++)
-	{
-		tmp.save();
-		tmp = i;
-	}
 
-	spk::cout << " --- Undoing ---" << std::endl;
-	spk::cout << "Value : " << tmp << " should be 3" << std::endl;
-	tmp.undo();
-	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
-	tmp.undo();
-	spk::cout << "Value : " << tmp << " should be 1" << std::endl;
-	tmp.undo();
-	spk::cout << "Value : " << tmp << " should be 0" << std::endl;
-	tmp.undo();
-	spk::cout << "Value : " << tmp << " should be -1" << std::endl;
+	test.startChronometer("test 1");
+	Sleep(30);
+	spk::cout << "First test of 30ms :"<< test.stopChronometer("test 1");
 
-	spk::cout << " --- Redoing ---" << std::endl;
-	tmp.redo();
-	spk::cout << "Value : " << tmp << " should be 0" << std::endl;
-	tmp.redo();
-	spk::cout << "Value : " << tmp << " should be 1" << std::endl;
-	tmp.redo();
-	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
-	tmp.redo();
-	spk::cout << "Value : " << tmp << " should be 3" << std::endl;
 
-	spk::cout << " --- Undoing ---" << std::endl;
-	tmp.undo();
-	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
+	test.startChronometer("test 2");
+	test.startChronometer("test 3");
+	Sleep(55);
+	spk::cout << "Second test of 55ms :"<< test.stopChronometer("test 2");
 
-	spk::cout << " --- New branch ---" << std::endl;
-	tmp = 42;
-	tmp.save();
-	spk::cout << "Value : " << tmp << " should be 42" << std::endl;
-	tmp.undo();
-	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
-	tmp.undo();
-	spk::cout << "Value : " << tmp << " should be 1" << std::endl;
+	Sleep(80);
+	spk::cout << "Third test of 135ms :"<< test.stopChronometer("test 3");
 
-	spk::cout << " --- Redoing ---" << std::endl;
-	tmp.redo();
-	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
-	tmp.redo();
-	spk::cout << "Value : " << tmp << " should be 42" << std::endl;
-
+		test.startChronometer("test 1");
+	Sleep(30);
+	spk::cout << "Forth test of 30ms :"<< test.stopChronometer("test 1");
 
 	return 0;
 }
