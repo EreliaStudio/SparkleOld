a6353e8550e133068de56b54078f0b6895df89c7
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-07-04 15:51:38 +0200

    Moving the implementation to the correct file

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -69,53 +69,54 @@ struct MyStruct : Memento
 
 int main()
 {
-	MyStruct tmp;
+	spk::StandardValue<int> tmp;
 
-	tmp.value = -1;
+	tmp = -1;
 	for (size_t i = 0; i < 4; i++)
 	{
 		tmp.save();
-		tmp.value = i;
+		tmp = i;
 	}
 
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << " --- Undoing ---" << std::endl;
+	spk::cout << "Value : " << tmp << " should be 3" << std::endl;
 	tmp.undo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
 	tmp.undo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 1" << std::endl;
 	tmp.undo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 0" << std::endl;
 	tmp.undo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be -1" << std::endl;
 
 	spk::cout << " --- Redoing ---" << std::endl;
 	tmp.redo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 0" << std::endl;
 	tmp.redo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 1" << std::endl;
 	tmp.redo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
 	tmp.redo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 3" << std::endl;
 
 	spk::cout << " --- Undoing ---" << std::endl;
 	tmp.undo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
 
 	spk::cout << " --- New branch ---" << std::endl;
-	tmp.value = 42;
+	tmp = 42;
 	tmp.save();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 42" << std::endl;
 	tmp.undo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
 	tmp.undo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 1" << std::endl;
 
 	spk::cout << " --- Redoing ---" << std::endl;
 	tmp.redo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 2" << std::endl;
 	tmp.redo();
-	spk::cout << "Value : " << tmp.value << std::endl;
+	spk::cout << "Value : " << tmp << " should be 42" << std::endl;
 
 
 	return 0;
