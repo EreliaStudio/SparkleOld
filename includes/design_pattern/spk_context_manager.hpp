fb0999d45fa0bb2eea7f75fd42a15c03b9b6044d
Neyune <ereali@student.42.fr>
2023-07-04 17:25:59 +0200

    adding third buffer context

--- includes/design_pattern/spk_context_manager.hpp
+++ includes/design_pattern/spk_context_manager.hpp
@@ -26,7 +26,7 @@ namespace spk
 			{
 				if (ContextManager::instance() == nullptr)
 					throw std::runtime_error("Context manager not initialized");
-				ContextManager::instance()->swapContext();
+				ContextManager::instance()->swapReadWriteContext();
 			}
 		};
 
@@ -39,26 +39,46 @@ namespace spk
 					throw std::runtime_error("Context manager not initialized");
 				return (*ContextManager::instance()->_readOnlyContext);
 			}
+			
+			static void swap()
+			{
+				if (ContextManager::instance() == nullptr)
+					throw std::runtime_error("Context manager not initialized");
+				ContextManager::instance()->swapReadOnlyContext();
+			}
 		};
 
 	protected:
 		std::recursive_mutex _bufferAccessMutex;
 		std::unique_ptr<TContextType> _readWriteContext;
 		std::unique_ptr<TContextType> _readOnlyContext;
+		std::unique_ptr<TContextType> _intermediaryBuffer;
+		bool _isSwapRequested;
+
 
 		template <typename... Args>
 		ContextManager(Args &&...p_args) :
 			_readWriteContext(std::make_unique<TContextType>(std::forward<Args>(p_args)...)),
-			_readOnlyContext(std::make_unique<TContextType>(std::forward<Args>(p_args)...))
+			_readOnlyContext(std::make_unique<TContextType>(std::forward<Args>(p_args)...)),
+			_intermediaryBuffer(std::make_unique<TContextType>(std::forward<Args>(p_args)...)), _isSwapRequested(false)
 		{
 			
 		}
 
-		void swapContext()
+		void swapReadWriteContext()
+		{
+			std::lock_guard<std::recursive_mutex> lock(_bufferAccessMutex);
+			
+			_readWriteContext.swap(_intermediaryBuffer);
+			_isSwapRequested = true;
+		}
+
+		void swapReadOnlyContext()
 		{
 			std::lock_guard<std::recursive_mutex> lock(_bufferAccessMutex);
 
-			_readWriteContext.swap(_readOnlyContext);
+			_readOnlyContext.swap(_intermediaryBuffer);
+			_isSwapRequested = false;
 		}
 
 	public:
