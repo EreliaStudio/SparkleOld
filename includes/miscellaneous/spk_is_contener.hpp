bd49c8941dd36930174ac8dcd6f800d867e7bb41
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-07-04 16:51:57 +0200

    Removing unnecessary comments

--- includes/miscellaneous/spk_is_contener.hpp
+++ includes/miscellaneous/spk_is_contener.hpp
@@ -3,11 +3,9 @@
 
 namespace spk
 {
-	// Définition de base pour tester si un type est un conteneur
 	template <typename T, typename = void>
 	struct IsContainer : std::false_type {};
 
-	// Spécialisation pour les types qui sont des conteneurs
 	template <typename T>
 	struct IsContainer<T, std::void_t<decltype(std::declval<T>().begin()), decltype(std::declval<T>().end()), typename T::value_type>> : std::true_type {};
 }
