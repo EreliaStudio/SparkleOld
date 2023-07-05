1001aabcf4b562a5aa7e7fe2db9026e22f932ec9
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-07-05 18:50:14 +0200

    Adding the ProfilerModule

--- includes/application/concrete/linux/spk_graphical_application.hpp
+++ includes/application/concrete/linux/spk_graphical_application.hpp
@@ -7,6 +7,7 @@
 #include "application/modules/spk_mouse_module.hpp"
 #include "application/modules/spk_keyboard_module.hpp"
 #include "application/modules/spk_widget_module.hpp"
+#include "application/modules/spk_profiler_module.hpp"
 
 namespace spk
 {
@@ -26,6 +27,7 @@ namespace spk
 		spk::WindowModule *_windowModule; ///< Window module instance.
 		spk::MouseModule *_mouseModule; ///< Mouse module instance.
 		spk::KeyboardModule *_keyboardModule; ///< Keyboard module instance.
+		spk::ProfilerModule *_profilerModule; ///< Profiler module instance.
 
 		spk::WidgetModule* _widgetModule; ///< Widget module instance.
 
@@ -62,6 +64,7 @@ namespace spk
 		{
 			_APIModule = new spk::APIModule();
 			_timeModule = new spk::TimeModule();
+			_profilerModule = new spk::ProfilerModule();
 			
 			_windowModule = new spk::WindowModule(_APIModule->windowQueue(), p_title, p_size);
 			
@@ -84,6 +87,7 @@ namespace spk
 			delete _windowModule;
 			delete _mouseModule;
 			delete _keyboardModule;
+			delete _profilerModule;
 
 			delete _widgetModule;
 		}
--- includes/application/concrete/windows/spk_graphical_application.hpp
+++ includes/application/concrete/windows/spk_graphical_application.hpp
@@ -8,6 +8,7 @@
 #include "application/modules/spk_keyboard_module.hpp"
 #include "application/modules/spk_widget_module.hpp"
 #include "application/modules/spk_system_module.hpp"
+#include "application/modules/spk_profiler_module.hpp"
 
 namespace spk
 {
@@ -29,6 +30,7 @@ namespace spk
 		spk::MouseModule *_mouseModule; ///< Mouse module instance.
 		spk::KeyboardModule *_keyboardModule; ///< Keyboard module instance.
 		spk::SystemModule *_systemModule; ///< System module instance.
+		spk::ProfilerModule *_profilerModule; ///< Profiler module instance.
 
 		spk::WidgetModule* _widgetModule; ///< Widget module instance.
 
@@ -73,6 +75,7 @@ namespace spk
 			
 			_systemModule = new spk::SystemModule(_APIModule->systemQueue(), this);
 			_timeModule = new spk::TimeModule();
+			_profilerModule = new spk::ProfilerModule();
 			
 			_windowModule = new spk::WindowModule(_APIModule->windowQueue(), p_title, p_size, _APIModule);
 			
@@ -96,6 +99,7 @@ namespace spk
 			delete _mouseModule;
 			delete _keyboardModule;
 			delete _systemModule;
+			delete _profilerModule;
 
 			delete _widgetModule;
 		}
