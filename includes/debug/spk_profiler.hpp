6a7f9a3e097f5ea567b0b6a611341a90b65a061a
boissy.jeremy@yahoo.fr <boissy.jeremy@yahoo.fr>
2023-07-05 14:46:39 +0200

    split hpp and cpp , add commenting on chronometer , timer, time metrics and profiler

--- includes/debug/spk_profiler.hpp
+++ includes/debug/spk_profiler.hpp
@@ -7,67 +7,77 @@
 #include "system/spk_chronometer.hpp"
 #include "design_pattern/spk_singleton.hpp"
 
+
 namespace spk
 {
 	namespace Debug
 	{
+		/**
+		 * \class Profiler
+		 * \brief A singleton class that handles profiling using various chronometers.
+		 *
+		 * This class is responsible for managing multiple chronometers, allowing the software
+		 * to keep track of time metrics for profiling purposes. As a singleton, only one instance
+		 * of this class can exist. This ensures consistency across the entire software.
+		 */
 		class Profiler : public spk::Singleton<Profiler>
 		{
 			friend class spk::Singleton<Profiler>;
 
 		private:
+
+			/**
+			 * \brief A map that associates each chronometer with a unique name.
+			 */
 			std::map<std::wstring, spk::Chronometer> _chronometers;
 
-			Profiler()
-			{
-				if (spk::Singleton<spk::TimeMetrics>::instance() == nullptr)
-				{
-					throw std::runtime_error("Profiler can't be launched without an application");
-				}
-			}
+			/**
+			 * \brief Private constructor to implement the Singleton pattern.
+			 *
+			 * This constructor checks whether a TimeMetrics instance has been created.
+			 * If not, it throws an error, since Profiler requires TimeMetrics.
+			 */
+			Profiler();
 
 		public:
 
-			const spk::Chronometer &chronometer(const std::wstring &p_name) const
-			{
-				if (_chronometers.count(p_name) == 0)
-				{
-					throw std::runtime_error("This Chronometer does not exist ");
-				}
-				return (_chronometers.at(p_name));
-			}
+			/**
+			 * \brief Retrieves the chronometer associated with a specific name.
+			 * \param p_name The name of the chronometer.
+			 * \throw std::runtime_error if no chronometer exists with the given name.
+			 * \return The specified chronometer.
+			 */
+			const spk::Chronometer &chronometer(const std::wstring &p_name) const;
 
-			void resetChronometer(const std::wstring &p_name)
-			{
-				if (_chronometers.count(p_name) == 0)
-				{
-					throw std::runtime_error("This Chronometer does not exist ");
-				}
-				_chronometers[p_name].reset();
-			}
+			/**
+			 * \brief Resets the chronometer associated with a specific name.
+			 * \param p_name The name of the chronometer.
+			 * \throw std::runtime_error if no chronometer exists with the given name.
+			 */
+			void resetChronometer(const std::wstring &p_name);
 
-			void startChronometer(const std::wstring &p_name)
-			{
-				_chronometers[p_name].start();
-			}
+			/**
+			 * \brief Starts the chronometer associated with a specific name.
+			 * \param p_name The name of the chronometer.
+			 */
+			void startChronometer(const std::wstring &p_name);
 
-			void resumeChronometer(const std::wstring &p_name)
-			{
-				if (_chronometers.count(p_name) == 0)
-				{
-					throw std::runtime_error("This Chronometer does not exist ");
-				}
-				_chronometers[p_name].resume();
-			}
+			/**
+			 * \brief Resumes the chronometer associated with a specific name.
+			 * \param p_name The name of the chronometer.
+			 * \throw std::runtime_error if no chronometer exists with the given name.
+			 */
+			void resumeChronometer(const std::wstring &p_name);
 
-			const long long stopChronometer(const std::wstring &p_name)
-			{
-				if (_chronometers.count(p_name) == 0)
-				{
-					throw std::runtime_error("This Chronometer does not exist ");
-				}
-				return (_chronometers[p_name].stop());
-			}
+			/**
+			 * \brief Stops the chronometer associated with a specific name.
+			 * \param p_name The name of the chronometer.
+			 * \throw std::runtime_error if no chronometer exists with the given name.
+			 * \return The time elapsed since the chronometer was started.
+			 */
+			const long long stopChronometer(const std::wstring &p_name);
 		};
 	}
 }
+
+
---
+++ src/debug/spk_profiler.cpp
@@ -0,0 +1,57 @@
+#pragma once
+#include "debug/spk_profiler.hpp"
+
+namespace spk
+{
+	namespace Debug
+	{
+		Profiler::Profiler()
+		{
+			if (spk::Singleton<spk::TimeMetrics>::instance() == nullptr)
+			{
+				throw std::runtime_error("Profiler can't be launched without an application");
+			}
+		}
+
+		const spk::Chronometer &Profiler::chronometer(const std::wstring &p_name) const
+		{
+			if (_chronometers.count(p_name) == 0)
+			{
+				throw std::runtime_error("This Chronometer does not exist ");
+			}
+			return (_chronometers.at(p_name));
+		}
+
+		void Profiler::resetChronometer(const std::wstring &p_name)
+		{
+			if (_chronometers.count(p_name) == 0)
+			{
+				throw std::runtime_error("This Chronometer does not exist ");
+			}
+			_chronometers[p_name].reset();
+		}
+
+		void Profiler::startChronometer(const std::wstring &p_name)
+		{
+			_chronometers[p_name].start();
+		}
+
+		void Profiler::resumeChronometer(const std::wstring &p_name)
+		{
+			if (_chronometers.count(p_name) == 0)
+			{
+				throw std::runtime_error("This Chronometer does not exist ");
+			}
+			_chronometers[p_name].resume();
+		}
+
+		const long long Profiler::stopChronometer(const std::wstring &p_name)
+		{
+			if (_chronometers.count(p_name) == 0)
+			{
+				throw std::runtime_error("This Chronometer does not exist ");
+			}
+			return (_chronometers[p_name].stop());
+		}
+	}
+}
