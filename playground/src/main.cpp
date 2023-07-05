b178a0035be5739f96193d2457c591f6f172d4f9
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-07-05 13:45:18 +0200

    🌐✨✅ Unicode json's string test

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,12 +1,23 @@
 #include "playground.hpp"
 
+void enableUnicode()
+{
+	std::locale::global(std::locale(""));
+	std::wcout.imbue(std::locale());
+	std::wcin.imbue(std::locale());
+	std::wcerr.imbue(std::locale());
+}
+
 int main()
 {
 	spk::JSON::File file;
 	std::vector<std::wstring> jsonFiles(spk::listFile(L"json", L".json"));
 
-	for (std::wstring filePath: jsonFiles)
+	enableUnicode();
+	for (std::wstring filePath : jsonFiles)
 	{
+		if (filePath != L"json/string.json")
+			continue;
 		file.load(filePath);
 		spk::cout << "File {" << filePath << "}: " << std::endl
 			<< file << std::endl;
