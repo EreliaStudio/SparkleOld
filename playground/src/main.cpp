852b59fdd67140dfe115288e3b8ac04886e0fd3e
Neyune <56516084+Neyune@users.noreply.github.com>
2023-07-05 08:20:36 +0000

    Add timemetrics to header , change main to work

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,11 +1,4 @@
 #include "playground.hpp"
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-#include <iostream>
-#include <cstdlib>
 
 class Memento
 {
@@ -80,12 +73,20 @@ struct MyStruct : Memento
 		std::map<std::string, spk::Chronometer> _chronometers;
 
 		public:
-        void startChronometer(const std::string & p_name)
+		Profiler()
+		{
+			if (spk::Singleton<spk::TimeMetrics>::instance() == nullptr)
+			{
+				throw std::runtime_error("Profiler can't be launched without an application");
+			}
+		}
+
+        void startChronometer(const std::wstring & p_name)
 		{
 			if (_chronometers.count(p_name) != 0 && _chronometers[p_name].duration() != 0)
-				{
-					 throw std::runtime_error("Tried to start an already active Chronometer");
-				}
+			{
+				throw std::runtime_error("Tried to start an already active Chronometer");
+			}
 			_chronometers[p_name].start();
 		}
 
@@ -93,11 +94,11 @@ struct MyStruct : Memento
 		{
 			if (_chronometers.count(p_name) == 0 )
 			{
-					throw std::runtime_error("This Chronometer does not exist ");
+				throw std::runtime_error("This Chronometer does not exist ");
 			}
 			if (_chronometers[p_name].duration() == 0)
 			{
-					throw std::runtime_error("This Chronometer is not started ");
+				throw std::runtime_error("This Chronometer is not started ");
 			}		
 			return(_chronometers[p_name].stop());
 		}
@@ -106,25 +107,35 @@ struct MyStruct : Memento
 
 int main()
 {
-	Profiler test;
+	spk::Application app(L"myApp", spk::Vector2Int(400,400));
 
 
-	test.startChronometer("test 1");
-	Sleep(30);
-	spk::cout << "First test of 30ms :"<< test.stopChronometer("test 1");
-
-
-	test.startChronometer("test 2");
-	test.startChronometer("test 3");
-	Sleep(55);
-	spk::cout << "Second test of 55ms :"<< test.stopChronometer("test 2");
-
-	Sleep(80);
-	spk::cout << "Third test of 135ms :"<< test.stopChronometer("test 3");
-
-		test.startChronometer("test 1");
-	Sleep(30);
-	spk::cout << "Forth test of 30ms :"<< test.stopChronometer("test 1");
-
-	return 0;
+	return app.run();
 }
+
+
+
+// int main()
+// {
+	
+// 	Profiler test;
+
+// 	test.startChronometer(L"test 1");
+// 	spk::TimeMetrics::instance()->sleep(30);
+// 	spk::cout << "First test of 30ms :"<< test.stopChronometer(L"test 1") << std::endl;
+
+
+// 	test.startChronometer(L"test 2");
+// 	test.startChronometer(L"test 3");
+// 	spk::TimeMetrics::instance()->sleep(55);
+// 	spk::cout << "Second test of 55ms :"<< test.stopChronometer(L"test 2") << std::endl;
+
+// 	spk::TimeMetrics::instance()->sleep(80);
+// 	spk::cout << "Third test of 135ms :"<< test.stopChronometer(L"test 3") << std::endl;
+
+// 	test.startChronometer(L"test 1");
+// 	spk::TimeMetrics::instance()->sleep(30);
+// 	spk::cout << "Forth test of 30ms :"<< test.stopChronometer(L"test 1") << std::endl;
+
+// 	return 0;
+// }
