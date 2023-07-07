43640b59bdb856c14a824f0d88c45104b41d97a7
Doxygen Generation <CI-CL@github.com>
2023-07-07 08:31:48 +0000

    CI - Update Doxygen documentation

--- docs/html/search/all_11.js
+++ docs/html/search/all_11.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['quit_173',['quit',['../classspk_1_1AbstractApplication.html#ac8f2a348cdbbf7eaf10e77953aa21204',1,'spk::AbstractApplication']]]
+  ['quit_172',['quit',['../classspk_1_1AbstractApplication.html#ac8f2a348cdbbf7eaf10e77953aa21204',1,'spk::AbstractApplication']]]
 ];
--- docs/html/search/all_14.js
+++ docs/html/search/all_14.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['thread_239',['Thread',['../classspk_1_1Thread.html',1,'spk::Thread'],['../classspk_1_1Thread.html#a7addb5ad82fee3d906e0a49cb3572f69',1,'spk::Thread::Thread(const std::wstring &amp;p_threadName, Funct &amp;&amp;p_func, Args &amp;&amp;...p_args)'],['../classspk_1_1Thread.html#afdc8fce9be9a5bc00dfdd9c7d149f588',1,'spk::Thread::Thread(LaunchMethod p_launchMethod, std::wstring p_threadName, Funct &amp;&amp;p_func, Args &amp;&amp;...p_args)']]],
-  ['threadsafequeue_240',['ThreadSafeQueue',['../classspk_1_1ThreadSafeQueue.html',1,'spk::ThreadSafeQueue&lt; T &gt;'],['../classspk_1_1ThreadSafeQueue.html#ad95a7d5bfe93b6d2146e64ee2a433b1b',1,'spk::ThreadSafeQueue::ThreadSafeQueue()=default'],['../classspk_1_1ThreadSafeQueue.html#a7ef0d2aea7e6ff8d3f80e4aba00855f0',1,'spk::ThreadSafeQueue::ThreadSafeQueue(const ThreadSafeQueue&lt; T &gt; &amp;)=delete']]],
-  ['threadsafequeue_3c_20job_20_3e_241',['ThreadSafeQueue&lt; Job &gt;',['../classspk_1_1ThreadSafeQueue.html',1,'spk']]],
-  ['threadsafequeue_3c_20systemmessage_20_3e_242',['ThreadSafeQueue&lt; SystemMessage &gt;',['../classspk_1_1ThreadSafeQueue.html',1,'spk']]],
-  ['time_243',['time',['../classspk_1_1TimeMetrics.html#ab2ec64db76c2329d780ef3138ea60611',1,'spk::TimeMetrics']]],
-  ['timemetrics_244',['TimeMetrics',['../classspk_1_1TimeMetrics.html',1,'spk']]],
-  ['timemodule_245',['TimeModule',['../classspk_1_1TimeModule.html',1,'spk::TimeModule'],['../classspk_1_1TimeModule.html#a927bb091d697cdde8e9a66b1d5bc2a98',1,'spk::TimeModule::TimeModule()']]],
-  ['timeout_246',['Timeout',['../classspk_1_1Timer.html#a68d5c57fd3cd82f4f97fe8a34bc227a0ac85a251cc457840f1e032f1b733e9398',1,'spk::Timer']]],
-  ['timer_247',['Timer',['../classspk_1_1Timer.html',1,'spk::Timer'],['../classspk_1_1Timer.html#a407d0e659a442be8f81043fc05d14982',1,'spk::Timer::Timer()']]],
-  ['totalduration_248',['totalDuration',['../classspk_1_1Timer.html#adbf9623ac0ac02f0bc97a835fe2a08d8',1,'spk::Timer']]]
+  ['thread_238',['Thread',['../classspk_1_1Thread.html',1,'spk::Thread'],['../classspk_1_1Thread.html#a7addb5ad82fee3d906e0a49cb3572f69',1,'spk::Thread::Thread(const std::wstring &amp;p_threadName, Funct &amp;&amp;p_func, Args &amp;&amp;...p_args)'],['../classspk_1_1Thread.html#afdc8fce9be9a5bc00dfdd9c7d149f588',1,'spk::Thread::Thread(LaunchMethod p_launchMethod, std::wstring p_threadName, Funct &amp;&amp;p_func, Args &amp;&amp;...p_args)']]],
+  ['threadsafequeue_239',['ThreadSafeQueue',['../classspk_1_1ThreadSafeQueue.html',1,'spk::ThreadSafeQueue&lt; T &gt;'],['../classspk_1_1ThreadSafeQueue.html#ad95a7d5bfe93b6d2146e64ee2a433b1b',1,'spk::ThreadSafeQueue::ThreadSafeQueue()=default'],['../classspk_1_1ThreadSafeQueue.html#a7ef0d2aea7e6ff8d3f80e4aba00855f0',1,'spk::ThreadSafeQueue::ThreadSafeQueue(const ThreadSafeQueue&lt; T &gt; &amp;)=delete']]],
+  ['threadsafequeue_3c_20job_20_3e_240',['ThreadSafeQueue&lt; Job &gt;',['../classspk_1_1ThreadSafeQueue.html',1,'spk']]],
+  ['threadsafequeue_3c_20systemmessage_20_3e_241',['ThreadSafeQueue&lt; SystemMessage &gt;',['../classspk_1_1ThreadSafeQueue.html',1,'spk']]],
+  ['time_242',['time',['../classspk_1_1TimeMetrics.html#ab2ec64db76c2329d780ef3138ea60611',1,'spk::TimeMetrics']]],
+  ['timemetrics_243',['TimeMetrics',['../classspk_1_1TimeMetrics.html',1,'spk']]],
+  ['timemodule_244',['TimeModule',['../classspk_1_1TimeModule.html',1,'spk::TimeModule'],['../classspk_1_1TimeModule.html#a927bb091d697cdde8e9a66b1d5bc2a98',1,'spk::TimeModule::TimeModule()']]],
+  ['timeout_245',['Timeout',['../classspk_1_1Timer.html#a68d5c57fd3cd82f4f97fe8a34bc227a0ac85a251cc457840f1e032f1b733e9398',1,'spk::Timer']]],
+  ['timer_246',['Timer',['../classspk_1_1Timer.html',1,'spk::Timer'],['../classspk_1_1Timer.html#a407d0e659a442be8f81043fc05d14982',1,'spk::Timer::Timer()']]],
+  ['totalduration_247',['totalDuration',['../classspk_1_1Timer.html#adbf9623ac0ac02f0bc97a835fe2a08d8',1,'spk::Timer']]]
 ];
