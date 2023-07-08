e51e2b442509d42e201c62d65cd9c3d1f8e5de80
EreliaCorp <98489062+EreliaCorp@users.noreply.github.com>
2023-07-08 10:39:52 +0000

    Allow random generator to work with floating point

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,32 +1,14 @@
 #include "playground.hpp"
 
 int main() {
-    srand(time(NULL));
-    // Création d'une instance de la classe Perlin
-    spk::Perlin perlin(rand());
+    spk::RandomGenerator<float> rng;
 
-    // Configuration de quelques paramètres
-    perlin.configureRange(0.0f, 100.0f);
-    perlin.configureFrequency(10.0f);
-    perlin.configurePersistance(0.5f);
-    perlin.configureOctave(3);
-
-    // Création d'un échantillon de valeurs
-    for (int x = 0; x < 20; ++x) {
-        for (int y = 0; y < 49; ++y) {
-            // Appel de la méthode sample
-            float sample = perlin.sample(static_cast<float>(x), static_cast<float>(y));
-
-            if (y != 0)
-                spk::cout << " ";
-            if (sample > 50)
-                spk::cout << "X";
-            else
-                spk::cout << ".";
-        }
-        spk::cout << std::endl;
+    rng.setDistributionRange(0.0f, 100.0f);
+    
+    for (size_t i = 0; i < 15; i++)
+    {
+        spk::cout << "RNG[" << i << "] -> " << rng() << std::endl;
     }
-    spk::cout << std::endl;
 
     return (0);
 }
