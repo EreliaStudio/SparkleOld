96281180a09f308f1634b3653b9d4f16afb6e603
Neyune <56516084+Neyune@users.noreply.github.com>
2023-07-11 19:55:31 +0200

    Creation command parser (#148)
    Creation of a basic command parser 
    Co-authored-by: Neyune

---
+++ includes/design_pattern/spk_command_parser.hpp
@@ -0,0 +1,73 @@
+#pragma once
+#include <map>
+#include <functional>
+#include <deque>
+#include <cstddef>
+#include <stdexcept>
+
+namespace spk
+{
+	/**
+	 * @brief The CommandParser class is responsible for parsing and processing commands.
+	 */
+	class CommandParser
+	{
+	public:
+		/**
+		 * @brief Type representing a command callback function.
+		 * The callback function takes a command parameter of type std::wstring.
+		 */
+		using CommandCallback = std::function<void(const std::wstring&)>;
+
+		std::wstring _commandsIndicator;            /**< The command indicator string. */
+		std::wstring _commandsSeparator;            /**< The command separator string. */
+		std::map<std::wstring, CommandCallback> _commandsCallback;    /**< Map storing command callbacks. */
+		CommandCallback _noCommandCallback;         /**< Callback function for when no command is found. */
+
+	public:
+		/**
+		 * @brief Default constructor for the CommandParser class.
+		 * Creates a CommandParser object with default settings.
+		 * The commands separator is set at space (" ");
+		 */
+		CommandParser();
+
+		/**
+		 * @brief Sets the command indicator.
+		 *
+		 * @param p_commandIndicator The command indicator to set.
+		 */
+		void setCommandsIndicator(const std::wstring& p_commandIndicator);
+
+
+		/**
+		 * @brief Sets the command separator.
+		 *
+		 * @param p_commandSeparator The command separator to set.
+		 */
+		void setCommandsSeparator(const std::wstring& p_commandIndicator);
+
+		/**
+		 * @brief Sets the callback function to be executed when no command is found.
+		 *
+		 * @param p_callback The callback function to set.
+		 */
+		void setNoCommandCallback(const CommandCallback& p_callback);
+
+		/**
+		 * @brief Adds a callback function for a specific command.
+		 *
+		 * @param p_command The command to associate the callback function with.
+		 * @param p_callback The callback function to associate with the command.
+		 */
+		void addCommandCallback(const std::wstring& p_command, const CommandCallback& p_callback);
+
+		/**
+		 * @brief Parses the command line and executes the corresponding callback function.
+		 *
+		 * @param p_commandLine The command line to parse.
+		 */
+		void parse(const std::wstring& p_commandLine);
+	};
+
+}
--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,21 +1,30 @@
 #include "playground.hpp"
 
+
 int main()
 {
-	spk::TranslationAtlas atlas;
-	std::vector<std::wstring> args = { L"key", L"key2", L"key3", L"key4",
-										L"key5", L"key6", L"key7", L"key8" };
+	CommandParser parser;
 
-	try
-	{
-		atlas.load(L"playground/src/en_EN.json");
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	parser.setCommandsIndicator(L"/");
+	
 
-	for (const auto& key : args)
-		spk::cout << atlas.get(key) << std::endl;
+	parser.setNoCommandCallback([&](const std::wstring& p_lineToExecute){spk::cout << L"Command not found : " << p_lineToExecute << std::endl;});
+	parser.addCommandCallback(L"add", [&](const std::wstring& p_lineToExecute){spk::cout << L"Launching the add command with [" << p_lineToExecute << L"]" << std::endl; });
+	parser.addCommandCallback(L"add1", [&](const std::wstring& p_lineToExecute){spk::cout << L"Launching the add1 command with [" << p_lineToExecute << L"]" << std::endl; });
+	parser.addCommandCallback(L"add2", [&](const std::wstring& p_lineToExecute){spk::cout << L"Launching the add2 command with [" << p_lineToExecute << L"]" << std::endl; });
+	parser.addCommandCallback(L"remove", [&](const std::wstring& p_lineToExecute){spk::cout << L"Launching the remove command with [" << p_lineToExecute << L"]" << std::endl; });
+
+	parser.parse(L"/add This is another test");
+	parser.parse(L"/add1 This is another with add1 test");
+	parser.parse(L"/add2 This is another with add2 test");
+	parser.parse(L"/remove This is a test with the remove command");
+	parser.parse(L"coucou This is a t/est without indicator");
+	parser.parse(L"/notValidCommand This is a test with an indicator, but with an invalid command key");
+
+
+	parser.parse(L"");
+	parser.parse(L"/add");
+	parser.parse(L"/removeThis is a test with the remove command");
+
 	return (0);
 }
