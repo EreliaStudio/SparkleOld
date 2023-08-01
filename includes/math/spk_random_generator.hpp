48f96c0da76d1db5d1454581ca2eee81703ab5e3
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-08-01 12:09:51 +0200

    Edit the random generator to stick to other generator methods

--- includes/math/spk_random_generator.hpp
+++ includes/math/spk_random_generator.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <random>
+#include "miscellaneous/JSON/spk_JSON_object.hpp"
 
 namespace spk
 {
@@ -39,15 +40,36 @@ namespace spk
 				_distribution()
 			{}
 
+
+			/**
+			 * @brief Constructor using a JSON::Object as input.
+			 * @param p_object The object to parse to create the RandomGenerator.
+			 */
+			RandomGenerator(const spk::JSON::Object& p_object) :
+				_generator(p_object[L"Seed"].as<long>()),
+				_distribution()
+			{
+
+			}
+
 			// Disallow copying
 			RandomGenerator(const RandomGenerator& p_other) = delete;
+
+			/**
+			 * @brief Set the seed of the number generator.
+			 * @param p_seed The new seed of the generator.
+			 */
+			void configureSeed(const uint64_t& p_seed)
+			{
+				_generator = std::mt19937(p_seed);
+			}
 
 			/**
 			 * @brief Set the range of the distribution.
 			 * @param p_min The lower bound of the distribution range.
 			 * @param p_max The upper bound of the distribution range.
 			 */
-			void setDistributionRange(const TGeneratedType& p_min, const TGeneratedType& p_max)
+			void configureRange(const TGeneratedType& p_min, const TGeneratedType& p_max)
 			{
 				_distribution.param(typename GeneratorType::param_type(p_min, p_max));
 			}
@@ -56,7 +78,7 @@ namespace spk
 			 * @brief Generate a random number.
 			 * @return A random number within the set distribution range.
 			 */
-			constexpr TGeneratedType operator()() { return _distribution(_generator); }
+			constexpr TGeneratedType sample() { return _distribution(_generator); }
 
 			/**
 			 * @brief Get the minimum value of the distribution range.
