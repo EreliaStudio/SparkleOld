3ec0e802423c77f7aa90556067916fb003e23051
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-08-14 16:41:35 +0200

    Adding content array commentaries

--- includes/data_structure/spk_content_array.hpp
+++ includes/data_structure/spk_content_array.hpp
@@ -4,16 +4,31 @@
 
 namespace spk
 {
+    /**
+     * @brief A template class representing a fixed-length array.
+     *
+     * This class wraps a fixed-length array and provides basic access functions.
+     * The length is defined at compile time.
+     *
+     * @tparam TType The type of elements in the array.
+     * @tparam TLenght The compile-time fixed length of the array.
+     */
 	template <typename TType, size_t TLenght>
 	struct ContentArray
 	{
 	public:
+        /// Represents the fixed length of the array.
 		static const size_t Lenght = TLenght;
 
 	protected:
+		/// The underlying fixed-length array.
 		TType _content[Lenght];
 
 	public:
+
+        /**
+         * @brief Default constructor. Initializes each element with its default constructor.
+         */
 		ContentArray()
 		{
 			for (size_t i = 0; i < Lenght; i++)
@@ -22,11 +37,24 @@ namespace spk
 			}
 		}
 
+        /**
+         * @brief Gets a pointer to the underlying array.
+         *
+         * @return Pointer to the underlying array.
+         */
 		TType* content()
 		{
 			return (_content);
 		}
 
+        /**
+         * @brief Overloaded array subscript operator.
+         *
+         * Provides access to the elements of the array. Throws an exception if the index is out of bounds.
+         *
+         * @param p_index The index of the element to access.
+         * @return Reference to the element at the specified index.
+         */
 		TType& operator[](size_t p_index)
 		{
 			if (p_index >= Lenght)
@@ -34,6 +62,14 @@ namespace spk
 			return (_content[p_index]);
 		}
 
+        /**
+         * @brief Overloaded array subscript operator (const version).
+         *
+         * Provides access to the elements of the array. Throws an exception if the index is out of bounds.
+         *
+         * @param p_index The index of the element to access.
+         * @return Const reference to the element at the specified index.
+         */
 		const TType& operator[](size_t p_index) const
 		{
 			if (p_index >= Lenght)
