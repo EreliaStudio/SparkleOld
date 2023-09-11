44e77ed974f32b27789afce37fae7bfa58ed3c7f
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-09-11 17:53:24 +0200

    now we have two recipes for both opengl and vulkan

---
+++ cmake/listGenericSources.cmake
@@ -0,0 +1,47 @@
+function(listGenericSources SOURCE_FILES_VAR)
+
+	# --- Common to both Linux and Windows
+	file(GLOB COMMON_SOURCE_FILES  
+		"${CMAKE_SOURCE_DIR}/src/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/basic_functions/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/input/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/system/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/network/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/miscellaneous/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/miscellaneous/JSON/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/application/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/application/modules/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/widget/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/data_structure/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/design_pattern/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/iostream/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/math/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/math/perlin/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/threading/*.cpp"
+		"${CMAKE_SOURCE_DIR}/src/debug/*.cpp"
+	)
+
+	# --- Specific Windows
+	if (WIN32)
+		file(GLOB SYSTEM_DEPENDENT_SOURCE_FILES
+			"${CMAKE_SOURCE_DIR}/src/basic_functions/windows/*.cpp"
+			"${CMAKE_SOURCE_DIR}/src/network/windows/*.cpp"
+			"${CMAKE_SOURCE_DIR}/src/input/windows/*.cpp"
+			"${CMAKE_SOURCE_DIR}/src/application/modules/windows/*.cpp"
+		)
+
+	# --- Specific Linux
+	elseif(LINUX)
+		file(GLOB SYSTEM_DEPENDENT_SOURCE_FILES
+			"${CMAKE_SOURCE_DIR}/src/basic_functions/linux/*.cpp"
+			"${CMAKE_SOURCE_DIR}/src/network/linux/*.cpp"
+			"${CMAKE_SOURCE_DIR}/src/input/linux/*.cpp"
+			"${CMAKE_SOURCE_DIR}/src/application/modules/linux/*.cpp"
+		)
+
+	endif()
+	
+	set(${SOURCE_FILES_VAR} ${COMMON_SOURCE_FILES} PARENT_SCOPE)
+	list(APPEND ${SOURCE_FILES_VAR} ${SYSTEM_DEPENDENT_SOURCE_FILES})
+
+endfunction()
