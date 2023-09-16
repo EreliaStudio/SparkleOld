d9ec86274c2e4e1182562d8408da2e5586d80709
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-09-16 10:36:04 +0200

    🚧 Enhancement of pointers.

--- src/application/spk_application.cpp
+++ src/application/spk_application.cpp
@@ -6,58 +6,44 @@ namespace spk
 {
 	void Application::setupJobs()
 	{
-		_renderContracts.push_back(addJob(L"Render IPS increment", [&]() { _profilerModule->increaseRenderIPS(); }));
-		_renderContracts.push_back(addJob(L"API message pulling", [&]() { _APIModule->pullMessage(); }));
-		_renderContracts.push_back(addJob(L"Window clearing", [&]() { _windowModule->clear(); }));
-		_renderContracts.push_back(addJob(L"Widget rendering", [&]() { _widgetModule->render(); }));
-		_renderContracts.push_back(addJob(L"Window rendering", [&]() { _windowModule->render(); }));
+		_renderContracts.push_back(addJob(L"Render IPS increment", [&]() { _profilerModule.increaseRenderIPS(); }));
+		_renderContracts.push_back(addJob(L"API message pulling", [&]() { _APIModule.pullMessage(); }));
+		_renderContracts.push_back(addJob(L"Window clearing", [&]() { _windowModule.clear(); }));
+		_renderContracts.push_back(addJob(L"Widget rendering", [&]() { _widgetModule.render(); }));
+		_renderContracts.push_back(addJob(L"Window rendering", [&]() { _windowModule.render(); }));
 
-		_updateContracts.push_back(addJob(L"Updater", L"Update IPS increment", [&]() { _profilerModule->increaseUpdateIPS(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"System message treatement", [&]() { _systemModule->treatMessage(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Time metrics update", [&]() { _timeModule->updateTimeMetrics(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Profiler update data", [&]() { _profilerModule->updateData(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Window treat message", [&]() { _windowModule->treatMessage(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Mouse treat message", [&]() { _mouseModule->treatMessage(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Keyboard treat message", [&]() { _keyboardModule->treatMessage(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Widget update", [&]() { _widgetModule->update(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Mouse update status", [&]() { _mouseModule->updateMouse(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Keyboard update status", [&]() { _keyboardModule->updateKeyboard(); }));
-		_updateContracts.push_back(addJob(L"Updater", L"Update IPS increment", [&]() { _profilerModule->increaseUpdateIPS(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Update IPS increment", [&]() { _profilerModule.increaseUpdateIPS(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"System message treatement", [&]() { _systemModule.treatMessage(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Time metrics update", [&]() { _timeModule.updateTimeMetrics(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Profiler update data", [&]() { _profilerModule.updateData(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Window treat message", [&]() { _windowModule.treatMessage(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Mouse treat message", [&]() { _mouseModule.treatMessage(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Keyboard treat message", [&]() { _keyboardModule.treatMessage(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Widget update", [&]() { _widgetModule.update(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Mouse update status", [&]() { _mouseModule.updateMouse(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Keyboard update status", [&]() { _keyboardModule.updateKeyboard(); }));
+		_updateContracts.push_back(addJob(L"Updater", L"Update IPS increment", [&]() { _profilerModule.increaseUpdateIPS(); }));
 	}
 
-	Application::Application(const std::wstring &p_title, const spk::Vector2Int &p_size)
-	{
-		_APIModule = new spk::APIModule();
-
-		_systemModule = new spk::SystemModule(_APIModule->systemQueue(), this);
-		_timeModule = new spk::TimeModule();
-		_profilerModule = new spk::ProfilerModule();
-
-		_windowModule = new spk::WindowModule(_APIModule->windowQueue(), p_title, p_size, _APIModule);
-
-		_mouseModule = new spk::MouseModule(_APIModule->mouseQueue());
-		_keyboardModule = new spk::KeyboardModule(_APIModule->keyboardQueue());
-
-		_widgetModule = new spk::WidgetModule();
-		
+	Application::Application(const std::wstring &p_title, const spk::Vector2Int &p_size) :
+		spk::AbstractApplication(),
+		_APIModule(),
+		_systemModule(_APIModule.systemQueue(), this),
+		_windowModule(_APIModule.windowQueue(), p_title, p_size, &_APIModule),
+		_mouseModule(_APIModule.mouseQueue()),
+		_keyboardModule(_APIModule.keyboardQueue()),
+		_widgetModule()
+	{		
 		resize(p_size);
 	}
 
 	Application::~Application()
 	{
-		delete _APIModule;
-		delete _systemModule;
-		delete _timeModule;
-		delete _windowModule;
-		delete _mouseModule;
-		delete _keyboardModule;
-		delete _profilerModule;
-		delete _widgetModule;
 	}
 	
-	spk::Widget::Canvas* Application::addCanvas(const std::filesystem::path& p_configurationFilePath)
+	std::shared_ptr<spk::Widget::Canvas> Application::addCanvas(const std::filesystem::path& p_configurationFilePath)
 	{
-		spk::Widget::Canvas* result = new spk::Widget::Canvas(p_configurationFilePath);
+		auto result = std::make_shared<spk::Widget::Canvas>(p_configurationFilePath);
 
 		result->setGeometry(spk::Vector2Int(0, 0), size());
 
