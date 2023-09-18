38fb9249c66f9d2ec937eac5e7adf39eb779257e
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-09-18 16:48:11 +0200

    🧱 Restructuration of includes.

--- cmake/listOpenGLIncludes.cmake
+++ cmake/listOpenGLIncludes.cmake
@@ -5,13 +5,13 @@ function(listOpenGLIncludes INCLUDES_TABLE_VAR)
 	# --- Specific Windows
 	if (WIN32)
 		set(OPENGL_SYSTEM_DEPENDENT_INCLUDES_FILES
-				"${CMAKE_SOURCE_DIR}/includes/graphics/surface/windows/opengl/spk_graphical_api_surface.hpp - graphics"
+				"${CMAKE_SOURCE_DIR}/includes/graphics/surface/opengl/windows/spk_graphical_api_surface.hpp - graphics"
 			)
 
 	# --- Specific Linux
 	elseif(UNIX)
 		set(OPENGL_SYSTEM_DEPENDENT_INCLUDES_FILES
-				"${CMAKE_SOURCE_DIR}/includes/graphics/surface/linux/opengl/spk_graphical_api_surface.hpp - graphics"
+				"${CMAKE_SOURCE_DIR}/includes/graphics/surface/opengl/linux/spk_graphical_api_surface.hpp - graphics"
 			)
 
 	endif()
