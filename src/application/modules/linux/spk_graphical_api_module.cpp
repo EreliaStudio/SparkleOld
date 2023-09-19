7b8149d307eeef38a78d24e9cc4eeea83c10883f
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-09-19 12:41:54 +0200

    ⚡️ Do not handle resize if the window only moved.

--- src/application/modules/linux/spk_graphical_api_module.cpp
+++ src/application/modules/linux/spk_graphical_api_module.cpp
@@ -9,19 +9,22 @@ namespace spk
 	{
 		switch (p_event->response_type & ~0x80)
 		{
-			case XCB_CONFIGURE_NOTIFY:
+		case XCB_CONFIGURE_NOTIFY:
+		{
+			xcb_configure_notify_event_t* configureNotifyEvent = reinterpret_cast<xcb_configure_notify_event_t*>(p_event);
+
+			spk::Vector2 ratio = spk::Vector2(
+				static_cast<float>(configureNotifyEvent->width) / Window::instance()->size().x,
+				static_cast<float>(configureNotifyEvent->height) / Window::instance()->size().y
+			);
+
+			if (Window::instance()->size().x != configureNotifyEvent->width || Window::instance()->size().y != configureNotifyEvent->height)
 			{
-				xcb_configure_notify_event_t* configureNotifyEvent = reinterpret_cast<xcb_configure_notify_event_t*>(p_event);
-
-				spk::Vector2 ratio = spk::Vector2(
-					static_cast<float>(configureNotifyEvent->width) / Window::instance()->size().x,
-					static_cast<float>(configureNotifyEvent->height) / Window::instance()->size().y
-				);
-
 				Window::instance()->setSize(spk::Vector2Int(configureNotifyEvent->width, configureNotifyEvent->height));
 				Widget::Atlas::instance()->resize(ratio);
-				break;
 			}
+			break;
+		}
 		}
 	}
 }
