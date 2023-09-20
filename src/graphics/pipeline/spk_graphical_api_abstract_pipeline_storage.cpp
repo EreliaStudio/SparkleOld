46aa6a182724bcc739a42a7ff342c85b1cf9d57f
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-09-20 18:22:19 +0200

    Spliting pipeline source into 3 files

---
+++ src/graphics/pipeline/spk_graphical_api_abstract_pipeline_storage.cpp
@@ -0,0 +1,10 @@
+#include "graphics/pipeline/spk_graphical_api_abstract_pipeline.hpp"
+
+namespace spk::GraphicalAPI
+{
+	AbstractPipeline::Object::Storage::Storage(const Storage::Configuration& p_storageConfiguration) :
+		_configuration(p_storageConfiguration)
+	{
+	}
+
+}
