613be88747d7c591961ec24dcb7243dd6fb7861d
ldutriez-Inspiron <ldutriez@student.42.fr>
2023-09-25 12:11:27 +0200

    🎨 Cleaning pipeline prototype and main file.

--- includes/graphics/pipeline/opengl/spk_graphical_api_pipeline.hpp
+++ includes/graphics/pipeline/opengl/spk_graphical_api_pipeline.hpp
@@ -7,9 +7,68 @@ namespace spk::GraphicalAPI
 {
 	class Pipeline : public spk::GraphicalAPI::AbstractPipeline
 	{
+	public:
+		class OpenGLObject : public spk::GraphicalAPI::AbstractPipeline::Object
+		{
+		private:
+			class Aggregator
+			{
+			private:
+				spk::GraphicalAPI::AbstractPipeline* _owner;
+				GLuint _VAO;
+
+			public:
+				Aggregator(spk::GraphicalAPI::AbstractPipeline* p_owner);
+
+				void activate();
+				void deactivate();
+			};
+
+			class Buffer
+			{
+			private:
+				GLuint _vbo;
+				GLenum _mode;
+				size_t _size = 0;
+				size_t _pushedSize = 0;
+
+			public:
+				Buffer(const spk::GraphicalAPI::AbstractPipeline::Object::Storage::Configuration& p_configuration);
+
+				void push(const void* data, const size_t dataSize);
+
+				void activate();
+				void deactivate();
+			};
+
+			Aggregator _aggregator;
+			Buffer _modelBuffer;
+			Buffer _indexesBuffer;
+
+		public:
+			OpenGLObject(spk::GraphicalAPI::AbstractPipeline* p_owner, const spk::GraphicalAPI::AbstractPipeline::Object::Storage::Configuration& p_storageConfiguration);
+
+			void push();
+
+			void activate();
+			void deactivate();
+		};
+
 	private:
+		GLuint _program;
+
+		void _loadProgram(
+			const std::string& p_vertexName, const std::string& p_vertexCode,
+			const std::string& p_fragmentName, const std::string& p_fragmentCode);
 
 	public:
+		Pipeline(const std::filesystem::path& p_vertexShaderPath, const std::filesystem::path& p_fragmentShaderPath);
 
+		void activate();
+		void deactivate();
+
+		void launch(const size_t& p_nbIndexes);
+
+		std::shared_ptr<Object> createObject();
 	};
 }
--- src/graphics/pipeline/opengl/spk_graphical_api_pipeline.cpp
+++ src/graphics/pipeline/opengl/spk_graphical_api_pipeline.cpp
@@ -3,5 +3,46 @@
 
 namespace spk::GraphicalAPI
 {
-	
+	Pipeline::Pipeline(const std::filesystem::path& p_vertexShaderPath, const std::filesystem::path& p_fragmentShaderPath) :
+		spk::GraphicalAPI::AbstractPipeline()
+	{
+		loadFromFile(p_vertexShaderPath, p_fragmentShaderPath);
+	}
+
+	void Pipeline::activate()
+	{
+		glUseProgram(_program);
+	}
+
+	void Pipeline::deactivate()
+	{
+		glUseProgram(0);
+	}
+
+	void Pipeline::launch(const size_t& p_nbIndexes)
+	{
+		glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(p_nbIndexes), GL_UNSIGNED_INT, nullptr);
+	}
+
+	std::shared_ptr<Pipeline::Object> Pipeline::createObject()
+	{
+		return (std::make_shared<OpenGLObject>(this, _storageConfiguration));
+	}
+
+	void Pipeline::_loadProgram(
+		const std::string& p_vertexName, const std::string& p_vertexCode,
+		const std::string& p_fragmentName, const std::string& p_fragmentCode)
+	{
+		_program = glCreateProgram();
+		GLuint _vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+		GLuint _fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+		spk::GraphicalAPI::compileShaderModule(_vertexShaderID, p_vertexCode);
+		spk::GraphicalAPI::compileShaderModule(_fragmentShaderID, p_fragmentCode);
+		spk::GraphicalAPI::compileProgram(_program, _vertexShaderID, _fragmentShaderID);
+
+		glDeleteShader(_vertexShaderID);
+		glDeleteShader(_fragmentShaderID);
+		activate();
+	}
 }
