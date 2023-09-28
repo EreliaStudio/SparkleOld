0999120384767f5d177743b9f4ab998cb784b394
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-09-28 13:46:40 +0200

    Moving configuration source into a folder and spliting it in multiple files

---
+++ src/graphics/pipeline/configuration/spk_graphical_api_abstract_pipeline_configuration_push_constant.cpp
@@ -0,0 +1,10 @@
+#include "graphics/pipeline/spk_graphical_api_abstract_pipeline.hpp"
+#include <regex>
+
+namespace spk::GraphicalAPI
+{
+	void parseShaderPushConstants(AbstractPipeline::Configuration& p_configuration, const std::string& p_shaderCode)
+	{
+
+	}
+}
