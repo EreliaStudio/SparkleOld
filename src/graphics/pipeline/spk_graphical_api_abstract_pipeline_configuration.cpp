5a05649f2b467dcc6312b475893a73fdb6c11b57
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-09-28 11:51:14 +0200

    Buffer seem to be parse correctly now

--- src/graphics/pipeline/spk_graphical_api_abstract_pipeline_configuration.cpp
+++ src/graphics/pipeline/spk_graphical_api_abstract_pipeline_configuration.cpp
@@ -3,9 +3,51 @@
 
 namespace spk::GraphicalAPI
 {
-	void parseShaderStorageBuffer(AbstractPipeline::Configuration& p_configuration, const std::string& p_shaderCode)
+	void loadShaderStorageBufferUnit(AbstractPipeline::Configuration& p_configuration, const std::string& p_locationInput, const std::string& p_typeInput)
 	{
+		auto it = p_configuration.dataTypes.find(p_typeInput);
+		if (it == p_configuration.dataTypes.end()) {
+			spk::throwException(L"Invalid data type: " + spk::to_wstring(p_typeInput));
+		}
+		AbstractPipeline::Configuration::Data dataType = it->second;
+
+		size_t newOffset = 0;
+		for (const auto& field : p_configuration.storage.fields) {
+			newOffset += field.attribute.size;
+		}
+
+		AbstractPipeline::Configuration::StorageLayout::Field newField(dataType, std::stoi(p_locationInput), newOffset);
+
+		p_configuration.storage.fields.push_back(newField);
+
+		p_configuration.storage.stride += dataType.size;
 	}
+
+	void parseShaderStorageBuffers(AbstractPipeline::Configuration& p_configuration, const std::string& p_shaderCode)
+    {
+        std::regex bufferRegex(R"(^.*layout\s*\(\s*location\s*=\s*(\d+)\s*\)\s*in\s+(\w+)\s+\w+\s*;.*$)", std::regex_constants::ECMAScript | std::regex_constants::icase);
+        std::regex inVariableRegex(R"(^.*\bin\b.*$)", std::regex_constants::ECMAScript | std::regex_constants::icase);
+
+        std::smatch match;
+        
+        std::string::const_iterator searchStart = p_shaderCode.cbegin();
+
+        while (std::regex_search(searchStart, p_shaderCode.cend(), match, inVariableRegex)) 
+        {
+            std::string matchedLine = match[0].str();
+            std::smatch bufferMatch;
+
+            if (!std::regex_search(matchedLine, bufferMatch, bufferRegex)) 
+                spk::throwException(L"Input variable in line [" + spk::to_wstring(matchedLine) + L"] lacks a layout location");
+
+			std::string locationValue = bufferMatch[1].str();
+			std::string typeValue = bufferMatch[2].str();
+			            
+			loadShaderStorageBufferUnit(p_configuration, locationValue, typeValue);
+
+            searchStart = match.suffix().first;
+        }
+    }
 
 	void parseShaderStructureBody(AbstractPipeline::Configuration& p_configuration, const std::string& p_structureName, const std::string& p_structureBody)
 	{
@@ -98,7 +140,7 @@ namespace spk::GraphicalAPI
 	AbstractPipeline::Configuration::Configuration(const std::string& p_vertexCode, const std::string& p_fragmentCode) :
 		Configuration()
 	{
-		parseShaderStorageBuffer(*this, p_vertexCode);
+		parseShaderStorageBuffers(*this, p_vertexCode);
 
 		parseShaderStructures(*this, p_vertexCode);
 		parseShaderStructures(*this, p_fragmentCode);
