d4d41e9fa3436993afea2e18c10b6593745e4bc7
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-10-08 17:20:17 +0200

    renaming playground files

--- playground/src/pipeline/configuration/spk_abstract_pipeline_configuration_data.cpp
+++ playground/src/pipeline/configuration/spk_abstract_pipeline_configuration_data.cpp
@@ -1,13 +1,13 @@
 #include "pipeline/pipeline.hpp"
 
 AbstractPipeline::Configuration::Data::Data(const Type &p_type, const size_t &p_format, const size_t &p_size) : type(p_type),
-																									 format(p_format),
-																									 size(p_size)
+																												format(p_format),
+																												size(p_size)
 {
 }
 
 AbstractPipeline::Configuration::Data::Data(const size_t &p_size) : type(Type::Structure),
-								   format(1),
-								   size(p_size)
+																	format(1),
+																	size(p_size)
 {
 }
