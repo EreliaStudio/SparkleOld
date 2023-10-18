15d0ac101884fc1c05f8825ad3e0e0c41f188c03
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-10-18 19:13:04 +0200

    Triangle seem to print out fine now

---
+++ src/graphics/pipeline/opengl/spk_pipeline_object_aggregator.cpp
@@ -0,0 +1,22 @@
+#include "graphics/pipeline/spk_pipeline.hpp"
+
+namespace spk
+{
+	Pipeline::Aggregator::Aggregator(spk::AbstractPipeline* p_owner) :
+		_owner(p_owner)
+	{
+		_owner->activate();
+		glGenVertexArrays(1, &_VAO);
+		activate();
+	}
+
+	void Pipeline::Aggregator::activate()
+	{
+		glBindVertexArray(_VAO);
+	}
+
+	void Pipeline::Aggregator::deactivate()
+	{
+		glBindVertexArray(0);
+	}
+}
