9e02c4377991d10f8c9319f2999326075eb73559
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-10-21 18:00:59 +0200

    Spliting windows frame into multiple files

--- includes/graphics/frame/windows/spk_frame.hpp
+++ includes/graphics/frame/windows/spk_frame.hpp
@@ -28,117 +28,27 @@ namespace spk
 
 		bool _wasResized = false;
 
-        void _convertTitle(const std::wstring &p_title)
-        {
-            _convertedTitle = new wchar_t[p_title.size() + 1];
-            for (size_t i = 0; i < p_title.size(); i++)
-            {
-                _convertedTitle[i] = p_title[i];
-            }
-            _convertedTitle[p_title.size()] = '\0';
-        }
+        void _convertTitle(const std::wstring &p_title);
+        void _createGhostInstance();
+        void _registerFrameClass();
+        void _createWindowFrame(void *p_APIModule, const Vector2UInt &p_size);
 
-        void _createGhostInstance()
-        {
-            _hInstance = GetModuleHandle(NULL);
-        }
+        void _activeWindow();
 
-        void _registerFrameClass()
-        {
-            _frameClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-            _frameClass.lpfnWndProc = (WNDPROC) WindowProc;
-            _frameClass.cbClsExtra = 0;
-            _frameClass.cbWndExtra = 0;
-            _frameClass.hInstance = _hInstance;
-            _frameClass.hIcon = LoadIcon(NULL, IDI_WINLOGO);
-            _frameClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-            _frameClass.hbrBackground = NULL;
-            _frameClass.lpszMenuName = NULL;
-            _frameClass.lpszClassName = _convertedTitle;
-
-            RegisterClassW(&_frameClass);
-        }
-
-        void _createWindowFrame(void *p_APIModule, const Vector2UInt &p_size)
-        {
-            _windowSize.left = (long)0;
-            _windowSize.right = (long)p_size.x;
-            _windowSize.top = (long)0;
-            _windowSize.bottom = (long)p_size.y;
-
-            _windowExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
-            _windowStyle = WS_OVERLAPPEDWINDOW;
-
-            AdjustWindowRectEx(&_windowSize, _windowStyle, FALSE, _windowExStyle);
-
-            _windowFrame = CreateWindowEx(_windowExStyle, (LPCTSTR)(_convertedTitle), (LPCTSTR)(_convertedTitle), WS_OVERLAPPEDWINDOW,
-                                          CW_USEDEFAULT, 0, _windowSize.right - _windowSize.left, _windowSize.bottom - _windowSize.top, NULL, NULL, _hInstance, p_APIModule);
-        }
-
-        void _activeWindow()
-        {
-            ShowWindow(_windowFrame, SW_SHOW);
-            UpdateWindow(_windowFrame);
-        }
-
-        void _onResize()
-        {
-            
-        }
-
-		void _onSetSize()
-		{
-			_wasResized = true;
-		}
+        void _onResize();
+		void _onSetSize();
 
     public:
-        Frame(const std::wstring &p_title, const Vector2UInt &p_size, void *p_ptr = nullptr) :
-            spk::AbstractFrame(p_size)
-        {
-            _convertTitle(p_title);
-
-            _createGhostInstance();
-
-            _registerFrameClass();
-
-            _createWindowFrame(p_ptr, p_size);
-
-            _activeWindow();
-        }
-
-        ~Frame()
-        {
-            delete _convertedTitle;
-        }
+        Frame(const std::wstring &p_title, const Vector2UInt &p_size, void *p_ptr = nullptr);
+        ~Frame();
         
-        HINSTANCE hInstance() const
-        {
-            return (_hInstance);
-        }
-        
-        HWND windowFrame() const
-        {
-            return (_windowFrame);
-        }
+        HINSTANCE hInstance() const;
+        HWND windowFrame() const;
 
-		bool wasResized() const
-		{
-			return (_wasResized);
-		};
+		bool wasResized() const;
+		void resizeHandled();
 
-		void resizeHandled()
-		{
-			_wasResized = false;
-		}
-
-        void render()
-        {
-
-        }
-
-        void clear()
-        {
-            InvalidateRect(_windowFrame, NULL, TRUE);
-        }
+        void render();
+        void clear();
     };
 }
