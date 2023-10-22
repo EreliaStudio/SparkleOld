caef00ad44943a4d155681b30289223142bd3d97
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-10-22 17:25:25 +0200

    Editing the function to follow our code rules

--- src/graphics/surface/opengl/spk_debug_callback.cpp
+++ src/graphics/surface/opengl/spk_debug_callback.cpp
@@ -18,16 +18,21 @@ namespace spk
 	 * @param p_message The actual debug message string.
 	 * @param p_userParam A user-defined parameter.
 	 */
-	void GLAPIENTRY MessageOpenGLCallback(GLenum source,
-										  GLenum type,
-										  GLuint id,
-										  GLenum severity,
-										  GLsizei length,
-										  const GLchar *message,
-										  const void *userParam)
+	void GLAPIENTRY MessageOpenGLCallback(GLenum p_source,
+										GLenum p_type,
+										GLuint p_id,
+										GLenum p_severity,
+										GLsizei p_length,
+										const GLchar *p_message,
+										const void *p_userParam)
 	{
-		if (severity == GL_DEBUG_TYPE_ERROR || severity == GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR || severity == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR ||
-			severity == GL_DEBUG_TYPE_ERROR_ARB || severity == GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB || severity == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB)
-			fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s", (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity, message);
+		// Check if the message is of a high severity level
+		if (p_severity == GL_DEBUG_TYPE_ERROR || p_severity == GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR || p_severity == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR ||
+			p_severity == GL_DEBUG_TYPE_ERROR_ARB || p_severity == GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR_ARB || p_severity == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR_ARB)
+		{
+			// Log the message to the standard error stream
+			fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s",
+					(p_type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), p_type, p_severity, p_message);
+		}
 	}
 }
