fbd45a305307a8e7e3e49f69845914227635de4b
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-10-22 13:05:46 +0200

    Commenting code from multiple graphics headers

--- includes/graphics/spk_color.hpp
+++ includes/graphics/spk_color.hpp
@@ -1,70 +1,78 @@
 #pragma once
 
+#include <iostream>  // for std::wostream
+
 namespace spk
 {
-	struct Color
-	{
-		float r;
-		float g;
-		float b;
-		float a;
+    /**
+     * @brief A structure representing RGBA color.
+     */
+    struct Color
+    {
+        /// Red component of the color.
+        float r;
+        /// Green component of the color.
+        float g;
+        /// Blue component of the color.
+        float b;
+        /// Alpha (transparency) component of the color.
+        float a;
 
-		Color() :
-			r(0),
-			g(0),
-			b(0),
-			a(1)
-		{
+        /**
+         * @brief Default constructor initializes color to transparent black.
+         */
+        Color();
 
-		}
-		Color(int p_value) : Color(p_value, p_value, p_value)
-		{
+        /**
+         * @brief Constructor to initialize color with grayscale value.
+         *
+         * @param p_value The grayscale value [0, 255].
+         */
+        Color(int p_value);
 
-		}
+        /**
+         * @brief Constructor to initialize color from integer values.
+         *
+         * @param p_r Red component [0, 255].
+         * @param p_g Green component [0, 255].
+         * @param p_b Blue component [0, 255].
+         * @param p_a Alpha component [0, 255] (default is 255).
+         */
+        Color(int p_r, int p_g, int p_b, int p_a = 255);
 
-		Color(int p_r, int p_g, int p_b, int p_a = 255) :
-			r(static_cast<float>(p_r) / 255.0f),
-			g(static_cast<float>(p_g) / 255.0f),
-			b(static_cast<float>(p_b) / 255.0f),
-			a(static_cast<float>(p_a) / 255.0f)
-		{
+        /**
+         * @brief Constructor to initialize color from float values.
+         *
+         * @param p_r Red component [0.0f, 1.0f].
+         * @param p_g Green component [0.0f, 1.0f].
+         * @param p_b Blue component [0.0f, 1.0f].
+         * @param p_a Alpha component [0.0f, 1.0f] (default is 1.0f).
+         */
+        Color(float p_r, float p_g, float p_b, float p_a = 1.0f);
 
-		}
-		Color(float p_r, float p_g, float p_b, float p_a = 1.0f) :
-			r(p_r),
-			g(p_g),
-			b(p_b),
-			a(p_a)
-		{
+        /**
+         * @brief Addition operator to add two colors.
+         *
+         * @param p_color The color to add to the current color.
+         * @return A new color resulting from the addition.
+         */
+        Color operator + (const Color& p_color) const;
 
-		}
+        /**
+         * @brief Subtraction operator to subtract a color from the current color.
+         *
+         * @param p_color The color to subtract from the current color.
+         * @return A new color resulting from the subtraction.
+         */
+        Color operator - (const Color& p_color) const;
 
-		Color operator + (const Color& p_color) const
-		{
-			Color result;
-
-			result.r = std::clamp(r + p_color.r, 0.0f, 1.0f);
-			result.g = std::clamp(g + p_color.g, 0.0f, 1.0f);
-			result.b = std::clamp(b + p_color.b, 0.0f, 1.0f);
-
-			return (result);
-		}
-
-		Color operator - (const Color& p_color) const
-		{
-			Color result;
-
-			result.r = std::clamp(r - p_color.r, 0.0f, 1.0f);
-			result.g = std::clamp(g - p_color.g, 0.0f, 1.0f);
-			result.b = std::clamp(b - p_color.b, 0.0f, 1.0f);
-
-			return (result);
-		}
-
-		friend std::wostream& operator << (std::wostream& p_os, const Color& p_values)
-		{
-			p_os << p_values.r << " / " << p_values.g << " / " << p_values.b << " / " << p_values.a;
-			return (p_os);
-		}
-	};
+        /**
+         * @brief Stream insertion operator to print the color values.
+         *
+         * @param p_os The output stream.
+         * @param p_values The color to print.
+         * @return The output stream.
+         */
+        friend std::wostream& operator << (std::wostream& p_os, const Color& p_values);
+    };
 }
