7c2f79461cb28cf252f02a416af85d6ac4945a17
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-10-22 17:45:34 +0200

    Commenting AbstractSurface class

--- includes/graphics/surface/spk_abstract_surface.hpp
+++ includes/graphics/surface/spk_abstract_surface.hpp
@@ -6,45 +6,69 @@ namespace spk
 {
     /**
      * @brief An abstract class representing a surface for rendering.
-     * 
+     *
      * This class serves as the base class for all surface types,
      * providing the essential interfaces for resizing, clearing, and rendering.
      */
-	class AbstractSurface
-	{
-	protected:
+    class AbstractSurface
+    {
+    protected:
         /// A pointer to the associated frame.
-		const spk::AbstractFrame* _frame;
+        const spk::AbstractFrame *_frame;
 
-	public:
+    public:
         /**
          * @brief Constructs an AbstractSurface associated with the given frame.
-         * 
+         *
          * @param p_frame A pointer to the frame to be associated with this surface.
          */
-		AbstractSurface(const spk::AbstractFrame* p_frame);
+        AbstractSurface(const spk::AbstractFrame *p_frame);
 
-        // Deleted copy constructor and copy assignment operator
-		AbstractSurface(const AbstractSurface&) = delete;
-		AbstractSurface& operator=(const AbstractSurface&) = delete;
+        /**
+         * @brief Deleted copy constructor for AbstractSurface.
+         *
+         * Copying instances of this class is not permitted to ensure that resources
+         * managed by AbstractSurface are not duplicated or improperly managed.
+         */
+        AbstractSurface(const AbstractSurface &) = delete;
 
-        // Default move constructor and move assignment operator
-		AbstractSurface(AbstractSurface&&) = default;
-		AbstractSurface& operator=(AbstractSurface&&) = default;
+        /**
+         * @brief Deleted copy assignment operator for AbstractSurface.
+         *
+         * Copy assignment is not permitted to ensure that resources managed by the
+         * AbstractSurface are not duplicated or improperly managed.
+         */
+        AbstractSurface &operator=(const AbstractSurface &) = delete;
+
+        /**
+         * @brief Default move constructor for AbstractSurface.
+         *
+         * Allows instances of this class to be moved, transferring ownership of resources
+         * from the source instance to the new instance.
+         */
+        AbstractSurface(AbstractSurface &&) = default;
+
+        /**
+         * @brief Default move assignment operator for AbstractSurface.
+         *
+         * Allows move assignment, transferring ownership of resources from the source
+         * instance to this instance, replacing the current resources.
+         */
+        AbstractSurface &operator=(AbstractSurface &&) = default;
 
         /**
          * @brief Virtual function for resizing the surface.
          */
-		virtual void resize() = 0;
+        virtual void resize() = 0;
 
         /**
          * @brief Virtual function for clearing the surface.
          */
-		virtual void clear() = 0;
+        virtual void clear() = 0;
 
         /**
          * @brief Virtual function for rendering to the surface.
          */
-		virtual void render() = 0;
-	};
+        virtual void render() = 0;
+    };
 }
