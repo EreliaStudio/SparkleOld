04a61a037a33e0ccbc73aa650b5948cdb0892595
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-11-12 23:10:05 +0100

    Adding the mesh class

---
+++ includes/engine/spk_mesh.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+#include "math/spk_vector2.hpp"
+#include "math/spk_vector3.hpp"
+
+namespace spk
+{
+	class Mesh
+	{
+	private:
+		struct Vertex
+		{
+			spk::Vector3 pos;
+			spk::Vector2 uv;
+			spk::Vector3 normal;
+
+			Vertex();
+			Vertex(const spk::Vector3& p_pos, const spk::Vector2& p_uv, const spk::Vector3& p_normal);
+		};
+
+		std::vector<spk::Vector3> _points;
+		std::vector<spk::Vector2> _uvs;
+		std::vector<spk::Vector3> _normals;
+
+		std::vector<Vertex> _vertices;
+		std::vector<unsigned int> _indexes;
+
+	public:
+		Mesh();
+
+		std::vector<spk::Vector3>& points();
+		const std::vector<spk::Vector3>& points() const;
+
+		std::vector<spk::Vector2>& uvs();
+		const std::vector<spk::Vector2>& uvs() const;
+
+		std::vector<spk::Vector3>& normals();
+		const std::vector<spk::Vector3>& normals() const;
+		
+		const std::vector<Vertex>& vertices() const;
+		const std::vector<unsigned int> indexes() const;
+
+		void addVertex(const size_t& p_pointIndex, const size_t& p_uvIndex, const size_t& p_normalIndex);
+		void addFace(const unsigned int& p_vertexIndexA, const unsigned int& p_vertexIndexB, const unsigned int& p_vertexIndexC);
+		void addFace(const unsigned int& p_vertexIndexA, const unsigned int& p_vertexIndexB, const unsigned int& p_vertexIndexC, const unsigned int& p_vertexIndexD);
+
+	};
+}
