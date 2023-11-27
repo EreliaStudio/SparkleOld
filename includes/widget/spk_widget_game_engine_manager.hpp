d1448d1b97b65080f7353aef7b3b57a63a45aa3f
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-11-27 14:32:20 +0100

    Commenting game engine widget

--- includes/widget/spk_widget_game_engine_manager.hpp
+++ includes/widget/spk_widget_game_engine_manager.hpp
@@ -5,21 +5,47 @@
 
 namespace spk
 {
-	class GameEngineManager : public spk::Widget::Interface
-	{
-	private:
-		bool _resetPerspectiveParameters = true;
-		std::shared_ptr<spk::GameEngine> _gameEngine = nullptr;
+	/**
+     * @brief Manages and integrates the GameEngine with the user interface.
+     *
+     * The GameEngineManager class is responsible for handling the game engine's lifecycle,
+     * updating its state, and rendering it within the context of a user interface widget.
+     */
+    class GameEngineManager : public spk::Widget::Interface
+    {
+    private:
+        bool _resetPerspectiveParameters = true; ///< Flag to reset perspective parameters.
+        std::shared_ptr<spk::GameEngine> _gameEngine = nullptr; ///< Pointer to the managed GameEngine instance.
 
-		void _onGeometryChange();
-		void _onRender();
-		bool _onUpdate();
+        void _onGeometryChange(); ///< Handles changes in geometry, such as window resizing.
+        void _onRender(); ///< Handles rendering of the game engine.
+        bool _onUpdate(); ///< Handles updating the state of the game engine.
 
-	public:
-		GameEngineManager(const std::wstring& p_name);
-		~GameEngineManager();
+    public:
+        /**
+         * @brief Constructs a new Game Engine Manager object.
+         *
+         * @param p_name The name of the game engine manager.
+         */
+        GameEngineManager(const std::wstring& p_name);
 
-		std::shared_ptr<spk::GameEngine> gameEngine() const;
-		void setGameEngine(std::shared_ptr<spk::GameEngine> p_gameEngine);
-	};
+        /**
+         * @brief Destroys the Game Engine Manager object.
+         */
+        ~GameEngineManager();
+
+        /**
+         * @brief Gets the currently managed game engine.
+         *
+         * @return std::shared_ptr<spk::GameEngine> Shared pointer to the managed game engine.
+         */
+        std::shared_ptr<spk::GameEngine> gameEngine() const;
+
+        /**
+         * @brief Sets the game engine to be managed.
+         *
+         * @param p_gameEngine Shared pointer to the game engine to be managed.
+         */
+        void setGameEngine(std::shared_ptr<spk::GameEngine> p_gameEngine);
+    };
 }
