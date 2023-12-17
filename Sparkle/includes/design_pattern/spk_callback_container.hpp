f1205ae69bcb07497e5ce9d9373df28465df79ce
Hyarius <Boissy.Jeremy@yahoo.fr>
2023-12-17 16:39:51 +0100

    Commenting on callback container and tree node

--- Sparkle/includes/design_pattern/spk_callback_container.hpp
+++ Sparkle/includes/design_pattern/spk_callback_container.hpp
@@ -6,45 +6,108 @@
 #include <stdexcept>
 #include <memory>
 
+/**
+ * @namespace spk
+ * @brief Namespace for the Sparkle library components.
+ */
 namespace spk
 {
-	class CallbackContainer
-	{
-	public:
-		using Callback = std::function<void()>;
-		using Container = std::list<Callback>;
+    /**
+     * @class CallbackContainer
+     * @brief Manages a collection of callback functions.
+     *
+     * This class allows for the subscription of callback functions which can be triggered collectively. 
+     * Callbacks are managed via Contracts, which control the lifecycle and amendments of the callbacks.
+     */
+    class CallbackContainer
+    {
+    public:
+        using Callback = std::function<void()>;
+        using Container = std::list<Callback>;
 
-	private:
-		Container _callbacks;
+    private:
+        Container _callbacks; /**< Container storing the callbacks. */
 
-	public:
+    public:
+        /**
+		 * @class Contract
+		 * @brief Represents a subscription contract for a callback within the CallbackContainer.
+		 *
+		 * This class manages the lifecycle of a callback within the CallbackContainer. It allows for
+		 * amendments to the callback and ensures proper cleanup upon destruction. This class follows
+		 * the rule of five, disabling copy semantics and providing move semantics for efficient 
+		 * resource management.
+		 */
 		class Contract {
 			friend class CallbackContainer;
 
 		private:
-			Container::iterator _iterator;
-			CallbackContainer::Container* _container;
-			bool _isOwner;
+			Container::iterator _iterator; /**< Iterator pointing to the specific callback in the container. */
+			CallbackContainer::Container* _container; /**< Pointer to the container holding the callback. */
+			bool _isOwner; /**< Flag indicating if this contract is the owner (responsible for) the callback. */
 
+			/**
+			 * @brief Private constructor used by CallbackContainer to create a Contract.
+			 * @param p_iterator Iterator to the callback in the container.
+			 * @param p_container Pointer to the container holding the callback.
+			 */
 			Contract(Container::iterator p_iterator, CallbackContainer::Container* p_container);
 
 		public:
+			/**
+			 * @brief Default constructor. Constructs an empty Contract.
+			 */
 			Contract();
 
 			Contract(const Contract&) = delete;
 			Contract& operator=(const Contract&) = delete;
 
+			/**
+			 * @brief Move constructor. Takes over management of another Contract's callback.
+			 * @param other Contract to move from.
+			 */
 			Contract(Contract&& other) noexcept;
+
+			/**
+			 * @brief Move assignment operator. Takes over management of another Contract's callback.
+			 * @param other Contract to move from.
+			 * @return Reference to this Contract.
+			 */
 			Contract& operator=(Contract&& other) noexcept;
 
+			/**
+			 * @brief Destructor. Unsubscribes the callback from the container if this contract is the owner.
+			 */
 			~Contract();
 
+			/**
+			 * @brief Amends the callback associated with this contract.
+			 * @param p_newCallback New callback function to replace the current one.
+			 */
 			void amend(std::function<void()> p_newCallback);
+
+			/**
+			 * @brief Resigns the contract, removing the callback from the container if this contract is the owner.
+			 */
 			void resign();
 		};
 
-		size_t size() const;
-		Contract subscribe(std::function<void()> p_callback);
-		void notify() const;
-	};
+        /**
+         * @brief Returns the number of callbacks currently subscribed.
+         * @return Size of the callback container.
+         */
+        size_t size() const;
+
+        /**
+         * @brief Subscribes a new callback to the container.
+         * @param p_callback The callback function to subscribe.
+         * @return A contract object that manages the subscribed callback.
+         */
+        Contract subscribe(std::function<void()> p_callback);
+
+        /**
+         * @brief Notifies (executes) all subscribed callbacks.
+         */
+        void notify() const;
+    };
 }
--- Sparkle/includes/design_pattern/spk_tree_node.hpp
+++ Sparkle/includes/design_pattern/spk_tree_node.hpp
@@ -7,6 +7,16 @@
 
 namespace spk
 {
+	/**
+     * @class TreeNode
+     * @brief A template class representing a node in a tree structure.
+     *
+     * @tparam TType The type of the value that the tree node holds.
+     *
+     * This class supports parent-child relationships, where each node can have multiple children
+     * but only a single parent. It also allows for custom callback functions to be executed 
+     * during linking (adding a child) and unlinking (removing a child) operations.
+     */
 	template <typename TType>
 	class TreeNode
 	{
@@ -17,11 +27,15 @@ namespace spk
 		using UnlinkingCallback = std::function<void()>;
 		
 	private:
-		Parent* _parent;
-		std::vector<TType*> _childrens;
-		LinkingCallback _linkingCallback;
-		UnlinkingCallback _unlinkingCallback;
+		Parent* _parent; /**< Pointer to the parent node. */
+        std::vector<TType*> _childrens; /**< Vector of pointers to the children nodes. */
+        LinkingCallback _linkingCallback; /**< Callback function for linking a child node. */
+        UnlinkingCallback _unlinkingCallback; /**< Callback function for unlinking a child node. */
 
+        /**
+         * @brief Internal method to insert a child into the children vector.
+         * @param p_child Pointer to the child node to be inserted.
+         */
 		void _insertChildren(Child* p_child)
 		{
 			_childrens.push_back(static_cast<TType*>(p_child));
@@ -31,12 +45,18 @@ namespace spk
 		}
 
 	public:
+        /**
+         * @brief Default constructor. Initializes the tree node with no parent.
+         */
 		TreeNode() : 
 			_parent(nullptr)
 		{
 
 		}
 
+        /**
+         * @brief Destructor. Unlinks from parent and removes itself from parent's children, if applicable.
+         */
 		~TreeNode()
 		{
 			if (_parent != nullptr)
@@ -46,6 +66,10 @@ namespace spk
 		TreeNode(const TreeNode& p_other) = delete;
 		TreeNode& operator =(const TreeNode& p_other) = delete;
 
+        /**
+         * @brief Move constructor. Moves the node and its relationships from another node.
+         * @param p_other The other TreeNode to move from.
+         */
 		TreeNode(TreeNode&& p_other)
 		{
 			if (p_other._parent != nullptr)
@@ -55,6 +79,11 @@ namespace spk
 			p_other.transferChildrens(this);
 		}
 
+        /**
+         * @brief Move assignment operator. Moves the node and its relationships from another node.
+         * @param p_other The other TreeNode to move from.
+         * @return Reference to this TreeNode.
+         */
 		TreeNode& operator =(TreeNode&& p_other)
 		{
 			if (p_other._parent != nullptr)
@@ -66,26 +95,46 @@ namespace spk
 			return (*this);
 		}
 
+        /**
+         * @brief Sets the linking callback function.
+         * @param p_callback The callback function to set for linking.
+         */
 		void setLinkingCallback(const LinkingCallback& p_callback)
 		{
 			_linkingCallback = p_callback;
 		}
 
+        /**
+         * @brief Sets the unlinking callback function.
+         * @param p_callback The callback function to set for unlinking.
+         */
 		void setUnlinkingCallback(const UnlinkingCallback& p_callback)
 		{
 			_unlinkingCallback = p_callback;
 		}
 
+        /**
+         * @brief Gets the parent of this node.
+         * @return Constant pointer to the parent node.
+         */
 		const TType* parent() const
 		{
 			return (static_cast<TType*>(_parent));
 		}
 
+        /**
+         * @brief Gets the children of this node.
+         * @return Vector of constant pointers to the children nodes.
+         */
 		const std::vector<TType*> childrens() const
 		{
 			return (_childrens);
 		}
 
+        /**
+         * @brief Adds a child node to this node.
+         * @param p_child Pointer to the child node to be added.
+         */
 		void addChild(Child* p_child)
 		{
 			if (p_child->_parent != nullptr)
@@ -93,6 +142,10 @@ namespace spk
 			_insertChildren(p_child);
 		}
 
+        /**
+         * @brief Removes a child node from this node.
+         * @param p_child Pointer to the child node to be removed.
+         */
 		void removeChild(Child* p_child)
 		{
 			auto it = std::find(_childrens.begin(), _childrens.end(), p_child);
@@ -105,6 +158,10 @@ namespace spk
 			}
 		}
 
+        /**
+         * @brief Transfers all children of this node to another parent node.
+         * @param p_newParent Pointer to the new parent node to which children will be transferred.
+         */
 		void transferChildrens(Parent* p_newParent)
 		{
 			for (size_t i = 0; i < _childrens.size(); i++)
